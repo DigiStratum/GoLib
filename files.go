7de6b0655cb9effca1fe73a9c0db26cd356e4a4f
skelly <keenskelly@gmail.com>
2018-10-25 21:51:21 -0700

    Add new files library

---
+++ files.go
@@ -0,0 +1,28 @@
+package golib
+
+/*
+
+File handling library functions
+
+*/
+
+import(
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"errors"
+)
+
+func ReadFileString(path string) (*string, error) {
+	tbuf, err := ReadFileBytes(path)
+	if nil != err { return nil, err }
+	return string(*tbuf), nil
+}
+
+func ReadFileBytes(path string) (*[]byte, error) {
+	var tbuf []byte
+	tbuf, err = ioutil.ReadFile(path)
+	if nil != err { return nil, errors.New(fmt.Sprintf("Error reading '%s'", path) }
+	return &tbuf, nil
+}
+
