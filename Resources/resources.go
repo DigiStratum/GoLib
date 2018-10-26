7692d5823400235f5677994dcc2cbd10529b21c4
skelly <keenskelly@gmail.com>
2018-10-26 08:14:53 -0700

    Add a follow-up TODO for ResourceMap

--- Resources/resources.go
+++ Resources/resources.go
@@ -11,6 +11,7 @@ type Resource struct {
 }
 
 // Map of resource path to the Resource and its properties
+// TODO: Add some supporting funcs to Resource to get a list of Resources below a given path (i.e. everything in a dir)
 type ResourceMap map[string]*Resource
 
 func NewResource() *Resource {
