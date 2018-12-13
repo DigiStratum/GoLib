a56d123cfd49c1646c44d838e23eb98c7675a231
skelly <keenskelly@gmail.com>
2018-12-13 09:25:15 -0800

    Change IterateCallback() callback function prototype
    Want to call back with a single KeyValuePair object rather than separate key/value strings; this opens the door for callbacks with more complex values than just string (i.e. interface) in the future such that the value type may be returned along with the data, etc);

--- hashmap.go
+++ hashmap.go
@@ -6,6 +6,8 @@ package golib
 This HashMap class wraps a basic Go map with essential helper functions to make life easier for
 dealing with simple key/value pair data.
 
+We're going to let this thing panic if we are not initialized (nil). That's the caller's bad.
+
 TODO: Put some multi-threaded protections around the accessors here
 
 */
@@ -23,47 +25,41 @@ func NewHashMap() *HashMap {
 }
 
 // Check whether this HashMap is empty (has no properties)
-// Explodes if hash is nil
 func (hash *HashMap) IsEmpty() bool {
 	return 0 == hash.Size()
 }
 
 // Get the number of properties in this HashMap
-// Explodes if hash is nil
 func (hash *HashMap) Size() int {
 	return len(*hash)
 }
 
 // Merge some additional configuration data on top of our own
-// Explodes if hash is nil
 func (hash *HashMap) Merge(inbound *HashMap) {
 	for k, v := range *inbound { (*hash)[k] = v }
 }
 
 // Set a single configuration element key to the specified value
-// Explodes if hash is nil
 func (hash *HashMap) Set(key string, value string) {
 	(*hash)[key] = value
 }
 
 // Get a single configuration element by key name
-// Explodes if hash is nil
 func (hash *HashMap) Get(key string) string {
 	str := ""
-	if val, ok := (*hash)[key]; ok { str = val }
+	val, ok := (*hash)[key]
+	if ok { str = val }
 	return str
 }
 
 // Check whether we have a configuration element by key name
 func (hash *HashMap) Has(key string) bool {
-	if nil == hash { return false }
 	_, ok := (*hash)[key];
 	return ok
 }
 
 // Check whether we have configuration elements for all the key names
 func (hash *HashMap) HasAll(keys *[]string) bool {
-	if (nil == hash) || (nil == keys) { return false }
 	for _, key := range *keys {
 		_, ok := (*hash)[key];
 		if ! ok { return false }
@@ -81,14 +77,12 @@ func (hash *HashMap) GetCopy() *HashMap {
 }
 
 // Iterate over the keys for this HashMap and call a callback for each
-// Explodes if hash is nil
 // ref: https://ewencp.org/blog/golang-iterators/index.html
-func (hash *HashMap) IterateCallback(callback func(key, value string)) {
-	for k, v := range *hash { callback(k, v) }
+func (hash *HashMap) IterateCallback(callback func(kvp KeyValuePair)) {
+	for k, v := range *hash { callback(KeyValuePair{ Key: k, Value: v}) }
 }
 
 // Iterate over the keys for this HashMap and send all the KeyValuePairs to a channel
-// Explodes if hash is nil
 // ref: https://ewencp.org/blog/golang-iterators/index.html
 // ref: https://blog.golang.org/pipelines
 func (hash *HashMap) IterateChannel() <-chan KeyValuePair {
