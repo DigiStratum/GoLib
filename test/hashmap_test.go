54d65f5875d83865fe4e209022db98eaf793bb38
skelly <keenskelly@gmail.com>
2018-12-12 20:11:40 -0800

    Add s'more unit tests for HashMap

--- test/hashmap_test.go
+++ test/hashmap_test.go
@@ -15,6 +15,8 @@ import(
 	lib "github.com/DigiStratum/GoLib"
 )
 
+// ----------------------------------------------------------------------------
+
 func GetCaller() string {
 	_, file, no, ok := runtime.Caller(2)
 	if ok { return fmt.Sprintf("@%s:%d", file, no) }
@@ -31,16 +33,52 @@ func ExpectInt(expect, actual int, t *testing.T) {
 	t.Errorf("%s\nExpect: '%d', Actual: '%d'", GetCaller(), expect, actual)
 }
 
-func TestThat_HashMap_IsEmptyWhenNew(t *testing.T) {
-	sut := lib.HashMap{}
+func ExpectBool(expect, actual bool, t *testing.T) {
+	if expect == actual { return }
+	t.Errorf("%s:\nExpect: '%t', Actual: '%t'", GetCaller(), expect, actual)
+}
+
+// ----------------------------------------------------------------------------
+
+func TestThat_HashMap_Size_Is0_WhenNew(t *testing.T) {
+	sut := lib.NewHashMap()
 	ExpectInt(0, sut.Size(), t)
 }
 
-func TestThat_HashMap_GetsSetValues(t *testing.T) {
-	sut := lib.HashMap{}
+func TestThat_HashMap_Size_Is1_WithOneSet(t *testing.T) {
+	sut := lib.NewHashMap()
+	sut.Set("rosie", "posey")
+	ExpectInt(1, sut.Size(), t)
+}
+
+func TestThat_HashMap_IsEmpty_IsTrue_WhenNew(t *testing.T) {
+	sut := lib.NewHashMap()
+	ExpectBool(true, sut.IsEmpty(), t)
+}
+
+func TestThat_HashMap_IsEmpty_IsFalse_WhenNonEmpty(t *testing.T) {
+	sut := lib.NewHashMap()
+	sut.Set("scooby", "dooby")
+	ExpectBool(false, sut.IsEmpty(), t)
+}
+
+func TestThat_HashMap_Get_ReturnsValue_ForSetKey(t *testing.T) {
+	sut := lib.NewHashMap()
 	key := "test_key"
 	value := "test_value"
 	sut.Set(key, value)
 	ExpectString(value, sut.Get(key), t)
+}
+
+func TestThat_HashMap_Get_ReturnsEmptyString_ForUnsetKey(t *testing.T) {
+	sut := lib.NewHashMap()
+	ExpectString("", sut.Get("boguskey"), t)
+}
+
+func TestThat_HashMap_Merge_AddsNothing_ForEmptyMaps(t *testing.T) {
+	sut := lib.NewHashMap()
+	otherMap := lib.NewHashMap()
+	sut.Merge(otherMap)
+	ExpectInt(0, sut.Size(), t)
 }
 
