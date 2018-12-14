605d32ca80fb1d70d3e1c7d10b0f768a424ce539
skelly <keenskelly@gmail.com>
2018-12-14 14:40:56 -0800

    Refactor to use Expect*() funs from GoTools

--- test/hashmap_test.go
+++ test/hashmap_test.go
@@ -11,61 +11,21 @@ We don't want to accidentally introduce a change that starts handing out referen
 allow the state to be modified directly by an outside party.
 
 */
+
 import(
 	"fmt"
 	"testing"
-	"runtime"
-	"reflect"
 
+	test "github.com/DigiStratum/GoTools/test"
 	lib "github.com/DigiStratum/GoLib"
 )
-
-// ----------------------------------------------------------------------------
-
-func GetCaller() string {
-	_, file, no, ok := runtime.Caller(2)
-	if ok { return fmt.Sprintf("@%s:%d", file, no) }
-	return ""
-}
-
-func ExpectString(expect, actual string, t *testing.T) {
-	if expect == actual { return }
-	t.Errorf("\n\n%s:\nExpect: '%s', Actual: '%s'", GetCaller(), expect, actual)
-}
-
-func ExpectInt(expect, actual int, t *testing.T) {
-	if expect == actual { return }
-	t.Errorf("\n\n%s\nExpect: '%d', Actual: '%d'", GetCaller(), expect, actual)
-}
-
-func ExpectBool(expect, actual bool, t *testing.T) {
-	if expect == actual { return }
-	t.Errorf("\n\n%s:\nExpect: '%t', Actual: '%t'", GetCaller(), expect, actual)
-}
-
-// ref: https://stackoverflow.com/questions/31595791/how-to-test-panics
-func ExpectPanic(t *testing.T) {
-	if r := recover(); r == nil {
-		t.Errorf("\n\n%s:\nExpect: Panic, Actual: Nope!", GetCaller())
-	}
-}
-
-func ExpectNil(value interface{}, t *testing.T) {
-	if value == nil || (reflect.ValueOf(value).Kind() == reflect.Ptr && reflect.ValueOf(value).IsNil()) {
-		return
-	}
-
-	t.Errorf("\n\n%s:\nExpect: nil, Actual: non-nil", GetCaller())
-}
-
-// ----------------------------------------------------------------------------
 
 func TestThat_HashMap_Size_Is0_WhenNew(t *testing.T) {
 	// Setup
 	sut := lib.NewHashMap()
 
 	// Verify
-	ExpectInt(0, sut.Size(), t)
+	test.ExpectInt(0, sut.Size(), t)
 }
 
 func TestThat_HashMap_Size_Is1_WithOneSet(t *testing.T) {
@@ -76,7 +36,7 @@ func TestThat_HashMap_Size_Is1_WithOneSet(t *testing.T) {
 	sut.Set("rosie", "posey")
 
 	// Verify
-	ExpectInt(1, sut.Size(), t)
+	test.ExpectInt(1, sut.Size(), t)
 }
 
 func TestThat_HashMap_IsEmpty_IsTrue_WhenNew(t *testing.T) {
@@ -84,7 +44,7 @@ func TestThat_HashMap_IsEmpty_IsTrue_WhenNew(t *testing.T) {
 	sut := lib.NewHashMap()
 
 	// Verify
-	ExpectBool(true, sut.IsEmpty(), t)
+	test.ExpectBool(true, sut.IsEmpty(), t)
 }
 
 func TestThat_HashMap_IsEmpty_IsFalse_WhenNonEmpty(t *testing.T) {
@@ -93,7 +53,7 @@ func TestThat_HashMap_IsEmpty_IsFalse_WhenNonEmpty(t *testing.T) {
 	sut.Set("scooby", "dooby")
 
 	// Verify
-	ExpectBool(false, sut.IsEmpty(), t)
+	test.ExpectBool(false, sut.IsEmpty(), t)
 }
 
 func TestThat_HashMap_Has_IsFalse_WhenKeyMissing(t *testing.T) {
@@ -101,7 +61,7 @@ func TestThat_HashMap_Has_IsFalse_WhenKeyMissing(t *testing.T) {
 	sut := lib.NewHashMap()
 
 	// Verify
-	ExpectBool(false, sut.Has("boguskey"), t)
+	test.ExpectBool(false, sut.Has("boguskey"), t)
 }
 
 func TestThat_HashMap_Has_IsTrue_WhenKeyExists(t *testing.T) {
@@ -113,7 +73,7 @@ func TestThat_HashMap_Has_IsTrue_WhenKeyExists(t *testing.T) {
 	sut.Set(key, "testvalue")
 
 	// Verify
-	ExpectBool(true, sut.Has(key), t)
+	test.ExpectBool(true, sut.Has(key), t)
 }
 
 func TestThat_HashMap_Get_ReturnsValue_ForSetKey(t *testing.T) {
@@ -126,7 +86,7 @@ func TestThat_HashMap_Get_ReturnsValue_ForSetKey(t *testing.T) {
 	sut.Set(key, value)
 
 	// Verify
-	ExpectString(value, sut.Get(key), t)
+	test.ExpectString(value, sut.Get(key), t)
 }
 
 func TestThat_HashMap_Get_ReturnsEmptyString_ForUnsetKey(t *testing.T) {
@@ -134,7 +94,7 @@ func TestThat_HashMap_Get_ReturnsEmptyString_ForUnsetKey(t *testing.T) {
 	sut := lib.NewHashMap()
 
 	// Verify
-	ExpectString("", sut.Get("boguskey"), t)
+	test.ExpectString("", sut.Get("boguskey"), t)
 }
 
 func TestThat_HashMap_Merge_AddsNothing_ForEmptyMaps(t *testing.T) {
@@ -146,7 +106,7 @@ func TestThat_HashMap_Merge_AddsNothing_ForEmptyMaps(t *testing.T) {
 	sut.Merge(otherMap)
 
 	// Verify
-	ExpectInt(0, sut.Size(), t)
+	test.ExpectInt(0, sut.Size(), t)
 }
 
 func TestThat_HashMap_Merge_AddsEntries_ForNonEmptyMaps(t *testing.T) {
@@ -161,10 +121,10 @@ func TestThat_HashMap_Merge_AddsEntries_ForNonEmptyMaps(t *testing.T) {
 	sut.Merge(other)
 
 	// Verify
-	ExpectBool(false, sut.IsEmpty(), t)
-	ExpectInt(1, sut.Size(), t)
-	ExpectBool(true, sut.Has(key), t)
-	ExpectString(value, sut.Get(key), t)
+	test.ExpectBool(false, sut.IsEmpty(), t)
+	test.ExpectInt(1, sut.Size(), t)
+	test.ExpectBool(true, sut.Has(key), t)
+	test.ExpectString(value, sut.Get(key), t)
 }
 
 func TestThat_HashMap_Merge_ChangesNothing_WhenNilForEmptySet(t *testing.T) {
@@ -175,12 +135,12 @@ func TestThat_HashMap_Merge_ChangesNothing_WhenNilForEmptySet(t *testing.T) {
 	sut.Merge(lib.NewHashMap())
 
 	// Verify
-	ExpectNil(sut, t)
+	test.ExpectNil(sut, t)
 }
 
 func TestThat_HashMap_Merge_Panics_WhenNilForNonEmptySet(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	other := lib.NewHashMap()
 	other.Set("beep", "boop")
 	var sut *lib.HashMap	// nil
@@ -191,7 +151,7 @@ func TestThat_HashMap_Merge_Panics_WhenNilForNonEmptySet(t *testing.T) {
 
 func TestThat_HashMap_Set_Panics_WhenNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	var sut *lib.HashMap	// nil
 
 	// Test
@@ -200,7 +160,7 @@ func TestThat_HashMap_Set_Panics_WhenNil(t *testing.T) {
 
 func TestThat_HashMap_IsEmpty_Panics_WhenNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	var sut *lib.HashMap	// nil
 
 	// Test
@@ -209,7 +169,7 @@ func TestThat_HashMap_IsEmpty_Panics_WhenNil(t *testing.T) {
 
 func TestThat_HashMap_Size_Panics_WhenNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	var sut *lib.HashMap	// nil
 
 	// Test
@@ -218,7 +178,7 @@ func TestThat_HashMap_Size_Panics_WhenNil(t *testing.T) {
 
 func TestThat_HashMap_Get_Panics_WhenNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	var sut *lib.HashMap	// nil
 
 	// Test
@@ -227,7 +187,7 @@ func TestThat_HashMap_Get_Panics_WhenNil(t *testing.T) {
 
 func TestThat_HashMap_Has_Panics_WhenNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	var sut *lib.HashMap	// nil
 
 	// Test
@@ -236,7 +196,7 @@ func TestThat_HashMap_Has_Panics_WhenNil(t *testing.T) {
 
 func TestThat_HashMap_HasAll_Panics_WhenNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	var sut *lib.HashMap	// nil
 
 	// Test
@@ -245,7 +205,7 @@ func TestThat_HashMap_HasAll_Panics_WhenNil(t *testing.T) {
 
 func TestThat_HashMap_HasAll_Panics_WhenKeysNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	sut := lib.NewHashMap()
 
 	// Test
@@ -258,7 +218,7 @@ func TestThat_HashMap_HasAll_ReturnsTrue_WhenKeysEmptySet(t *testing.T) {
 	keys := make([]string, 0)
 
 	// Test
-	ExpectBool(true, sut.HasAll(&keys), t)
+	test.ExpectBool(true, sut.HasAll(&keys), t)
 }
 
 func TestThat_HashMap_HasAll_ReturnsFalse_WhenAnyKeyMissing(t *testing.T) {
@@ -268,7 +228,7 @@ func TestThat_HashMap_HasAll_ReturnsFalse_WhenAnyKeyMissing(t *testing.T) {
 	keys[0] = "missingkey"
 
 	// Test
-	ExpectBool(false, sut.HasAll(&keys), t)
+	test.ExpectBool(false, sut.HasAll(&keys), t)
 }
 
 func TestThat_HashMap_HasAll_ReturnsTrue_WhenAllKeysExist(t *testing.T) {
@@ -281,7 +241,7 @@ func TestThat_HashMap_HasAll_ReturnsTrue_WhenAllKeysExist(t *testing.T) {
 	keys[1] = "okey"
 
 	// Test
-	ExpectBool(true, sut.HasAll(&keys), t)
+	test.ExpectBool(true, sut.HasAll(&keys), t)
 }
 
 func TestThat_HashMap_Set_OverwritesValue_ForExistingKey(t *testing.T) {
@@ -295,7 +255,7 @@ func TestThat_HashMap_Set_OverwritesValue_ForExistingKey(t *testing.T) {
 	sut.Set(key, expected)
 
 	// Verify
-	ExpectString(expected, sut.Get(key), t)
+	test.ExpectString(expected, sut.Get(key), t)
 }
 
 func TestThat_HashMap_GetCopy_ReturnsNil_WhenNil(t *testing.T) {
@@ -303,7 +263,7 @@ func TestThat_HashMap_GetCopy_ReturnsNil_WhenNil(t *testing.T) {
 	var sut *lib.HashMap	// nil
 
 	// Verify
-	ExpectNil(sut.GetCopy(), t)
+	test.ExpectNil(sut.GetCopy(), t)
 }
 
 func TestThat_HashMap_GetCopy_ReturnsEmpty_WhenEmpty(t *testing.T) {
@@ -314,7 +274,7 @@ func TestThat_HashMap_GetCopy_ReturnsEmpty_WhenEmpty(t *testing.T) {
 	res := sut.GetCopy()
 
 	// Verify
-	ExpectInt(0, res.Size(), t)
+	test.ExpectInt(0, res.Size(), t)
 }
 
 func TestThat_HashMap_GetCopy_ReturnsNonEmpty_WhenNonEmpty(t *testing.T) {
@@ -331,17 +291,17 @@ func TestThat_HashMap_GetCopy_ReturnsNonEmpty_WhenNonEmpty(t *testing.T) {
 	res := sut.GetCopy()
 
 	// Verify
-	ExpectInt(num, res.Size(), t)
+	test.ExpectInt(num, res.Size(), t)
 	for i := 0; i < num; i++ {
 		key := fmt.Sprintf("key-%d", i)
 		value := fmt.Sprintf("value-%d", i)
-		ExpectString(value, res.Get(key), t)
+		test.ExpectString(value, res.Get(key), t)
 	}
 }
 
 func TestThat_HashMap_IterateCallback_Panics_WhenNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	var sut *lib.HashMap	// nil
 
 	// Test
@@ -357,7 +317,7 @@ func TestThat_HashMap_IterateCallback_MakesNoCalls_WhenEmpty(t *testing.T) {
 	sut.IterateCallback(func (kvp lib.KeyValuePair) { callbackCounter++ })
 
 	// Verify
-	ExpectInt(0, callbackCounter, t)
+	test.ExpectInt(0, callbackCounter, t)
 }
 
 func TestThat_HashMap_IterateCallback_MakesOneCallPerKey_WhenNonEmpty(t *testing.T) {
@@ -386,10 +346,10 @@ func TestThat_HashMap_IterateCallback_MakesOneCallPerKey_WhenNonEmpty(t *testing
 	})
 
 	// Verify
-	ExpectInt(num, len(callbacks), t)
+	test.ExpectInt(num, len(callbacks), t)
 	for i := 0; i < num; i++ {
-		ExpectInt(1, callbacks[keys[i]].Num, t)
-		ExpectString(sut.Get(keys[i]), callbacks[keys[i]].Value, t)
+		test.ExpectInt(1, callbacks[keys[i]].Num, t)
+		test.ExpectString(sut.Get(keys[i]), callbacks[keys[i]].Value, t)
 	}
 }
 
@@ -397,7 +357,7 @@ func TestThat_HashMap_IterateCallback_MakesOneCallPerKey_WhenNonEmpty(t *testing
 
 func TestThat_HashMap_IterateChannel_Panics_WhenNil(t *testing.T) {
 	// Setup
-	defer ExpectPanic(t)
+	defer test.ExpectPanic(t)
 	var sut *lib.HashMap	// nil
 
 	// Test
@@ -413,7 +373,7 @@ func TestThat_HashMap_IterateChannel_YieldsNoEntries_WhenEmpty(t *testing.T) {
 	for _ = range sut.IterateChannel() { entryCounter++ }
 
 	// Verify
-	ExpectInt(0, entryCounter, t)
+	test.ExpectInt(0, entryCounter, t)
 }
 
 func TestThat_HashMap_IterateChannel_YieldsOneEntryPerKey_WhenNonEmpty(t *testing.T) {
@@ -442,10 +402,10 @@ func TestThat_HashMap_IterateChannel_YieldsOneEntryPerKey_WhenNonEmpty(t *testin
 	}
 
 	// Verify
-	ExpectInt(num, len(entries), t)
+	test.ExpectInt(num, len(entries), t)
 	for i := 0; i < num; i++ {
-		ExpectInt(1, entries[keys[i]].Num, t)
-		ExpectString(sut.Get(keys[i]), entries[keys[i]].Value, t)
+		test.ExpectInt(1, entries[keys[i]].Num, t)
+		test.ExpectString(sut.Get(keys[i]), entries[keys[i]].Value, t)
 	}
 }
 
