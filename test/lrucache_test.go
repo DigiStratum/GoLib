2986a2ad82e512fd61ecf7a52fd973ba9587513f
skelly <keenskelly@gmail.com>
2018-12-18 21:23:12 -0800

    Add more LRU Cache unit test coverage

--- test/lrucache_test.go
+++ test/lrucache_test.go
@@ -22,6 +22,23 @@ func TestThat_LRUCache_Size_Is0_WhenNew(t *testing.T) {
 	ExpectInt(0, sut.Size(), t)
 }
 
+func TestThat_LRUCache_Size_IsCorrect_WithSomeEntries(t *testing.T) {
+	// Setup
+	sut := lib.NewLRUCache()
+
+	// Test
+	expectedSize := 0;
+	for i := 0; i < 10; i++ {
+		key := fmt.Sprintf("key%d", i)
+		content := fmt.Sprintf("content--%d", i)
+		expectedSize += len(content)
+		ExpectTrue(sut.Set(key, content), t)
+	}
+
+	// Verify
+	ExpectInt(expectedSize, sut.Size(), t)
+}
+
 func TestThat_LRUCache_Count_Is0_WhenNew(t *testing.T) {
 	// Setup
 	sut := lib.NewLRUCache()
@@ -122,6 +139,66 @@ func TestThat_LRUCache_SetLimits_LimitsCount_WhenSetAddsEntries(t *testing.T) {
 	for i := 1; i < countLimit; i++ { // We expect the lowest (oldest one) is replaced with the newest
 		key := fmt.Sprintf("key%d", i)
 		content := fmt.Sprintf("content%d", i)
+		ExpectTrue(sut.Has(key), t)
+		res := sut.Get(key)
+		ExpectString(content, *res, t)
+	}
+}
+
+func TestThat_LRUCache_SetLimits_LimitsSize_WhenSetAddsEntries(t *testing.T) {
+	// Setup
+	sut := lib.NewLRUCache()
+
+	// Test
+	count := 5
+	sizeLimit := (count - 1) * 10	// Limit size at 10 chars * our count, less one
+	sut.SetLimits(sizeLimit, 0)
+	for i := 0; i < count; i++ {
+		key := fmt.Sprintf("key%d", i)
+		content := fmt.Sprintf("content--%d", i)
+		ExpectTrue(sut.Set(key, content), t)
+	}
+
+	// Verify
+	ExpectInt(count - 1, sut.Count(), t) // should only have count limit less one
+	ExpectFalse(sut.Has("key0"), t)
+	for i := 1; i < count; i++ { // We expect the lowest (oldest one) is replaced with the newest
+		key := fmt.Sprintf("key%d", i)
+		content := fmt.Sprintf("content--%d", i)
+		ExpectTrue(sut.Has(key), t)
+		res := sut.Get(key)
+		ExpectString(content, *res, t)
+	}
+}
+
+func TestThat_LRUCache_SetLimits_LimitsCountAndSize_WhenSetAddsEntries(t *testing.T) {
+	// Setup
+	sut := lib.NewLRUCache()
+
+	// Test
+	countLimit := 5
+	sizeLimit := (countLimit - 1) * 10	// Limit size at 10 chars * our count, less one
+	sut.SetLimits(sizeLimit, countLimit - 1) // count limit is one less than we want
+	for i := 0; i < countLimit; i++ {
+		key := fmt.Sprintf("key%d", i)
+		content := fmt.Sprintf("content--%d", i)
+		ExpectTrue(sut.Set(key, content), t)
+	}
+	// Drop in a double-sized item which should displace two regular ones
+	expectedKey := fmt.Sprintf("key%d", countLimit)
+	expectedContent := "12345678901234567890"
+	ExpectTrue(sut.Set(expectedKey, expectedContent), t)
+	ExpectTrue(sut.Has(expectedKey), t)
+	res := sut.Get(expectedKey)
+	ExpectString(expectedContent, *res, t)
+
+	// Verify
+	ExpectInt(countLimit - 1, sut.Count(), t) // should only have count limit less TWO
+	ExpectFalse(sut.Has("key0"), t)
+	ExpectFalse(sut.Has("key1"), t)
+	for i := 2; i < countLimit; i++ { // We expect the lowest (oldest one) is replaced with the newest
+		key := fmt.Sprintf("key%d", i)
+		content := fmt.Sprintf("content--%d", i)
 		ExpectTrue(sut.Has(key), t)
 		res := sut.Get(key)
 		ExpectString(content, *res, t)
