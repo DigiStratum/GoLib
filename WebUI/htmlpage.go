d9ebf9c23f47782c923db3ea8636a5fbd0469953
Sean Kelly <keenskelly@gmail.com>
2019-11-20 22:55:29 -0800

    WIP HtmlPage rendering

--- WebUI/htmlpage.go
+++ WebUI/htmlpage.go
@@ -7,6 +7,8 @@ import(
 type HtmlPage struct {
 	document		string
 	scheme			*Scheme
+	dictionary		map[string]string
+	renderedDocument	string
 }
 
 // Make a new one of these
@@ -14,6 +16,20 @@ func NewHtmlPage() *HtmlPage {
 	lib.GetLogger().Trace("NewHtmlPage()")
 	return &HtmlPage{
 		document: "",
+		scheme: nil,
+		dictionary: make(map[string]string),
+		renderedDocument: nil,
 	}
+}
+
+func (page *HtmlPage) GetRenderedDocument() string {
+	if nil == page.renderedDocument {
+		page.renderDocument()
+	}
+	return page.renderedDocument
+}
+
+func (page *HtmlPage) renderDocument() {
+	page.renderedDocument = document
 }
 
