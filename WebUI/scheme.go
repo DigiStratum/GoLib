52be46a2e841bf86e69c35cf1b7f3fc0ec7779ec
Sean Kelly <keenskelly@gmail.com>
2019-11-27 22:26:28 -0800

    WIP fill in scheme implementaiton

--- WebUI/scheme.go
+++ WebUI/scheme.go
@@ -6,17 +6,32 @@ import(
 
 type Scheme struct {
 	layout		*Layout,
-	fragments	map[string]Fragment,	// key is Fragment.name
-	styles		map[string]Style,	// key is Style.selector
+	fragments	map[string]*Fragment,	// key is Fragment.name
+	stylesheet	[]*Style,
 }
 
 // Make a new one of these
 func NewScheme() *Scheme {
 	lib.GetLogger().Trace("NewScheme()")
 	return &Scheme{
-		layout: null,
-		fragments: make(map[string]Fragment),
-		styles: make(map[string]Style),
+		layout: nil,
+		fragments: make(map[string]*Fragment),
+		styles: make([]*Style),
 	}
 }
+
+// Set the layout for this scheme
+func (scheme *Scheme) SetLayout(layout *Layout) {
+	scheme.layout = layout
+}
+
+// Add a fragment to this scheme (should be referenced either by the layout or another fragment)
+func (scheme *Scheme) AddFragment(fragment *Fragment) {
+	scheme.fragments[fragment.Name] = fragment
+}
+
+// Add a style to this scheme's stylesheet
+func (scheme *Scheme) AddStyle(style *Style) {
+	scheme.stylesheet = append(scheme.stylesheet, style)
+
 
