cd08b4307f2fd6369b95bb3dff1d5f4599cbba51
Sean Kelly <keenskelly@gmail.com>
2020-01-28 19:47:41 -0800

    Add support for Named ObjectStores into ObjectStoreManager
    This gives us two methods of accessing Objects from the collection of ObjectStores: either by scanning UP the list as originally, or by the consumer specifying which ObjectStore to use, by name!

--- Objects/objectstoremanager.go
+++ Objects/objectstoremanager.go
@@ -2,10 +2,15 @@ package objects
 
 /*
 
-ObjectStore Manager provides an interface to one or more ObjectStores, in sequence, to find and
-access Objects by path. By scanning UP the list of ObjectStores, starting at 0, whichever
-ObjectStore matches the Object path first wins; this is a short-circuit model whereby the match
-closest to index 0 will return immediately without consideration for anything deeper.
+ObjectStore Manager provides an interface to a collection of ObjectStores to find and access Objects
+by path. There are two methods by which an ObjectStore in the collection may be accessed:
+
+A) Scan Method: By scanning UP the list of ObjectStores, starting at 0, whichever ObjectStore matches
+the Object path first wins; this is a short-circuit model whereby the match closest to index 0 will
+return immediately without consideration for anything deeper
+
+B) Name Method: By supplying a unique name to the ObjectStore such that it can be addressed by name
+directly and without scanning as above
 
 */
 
@@ -16,26 +21,30 @@ import (
 )
 
 type ObjectStoreManager struct {
-	// Ordered list of ObjectStores to find Objects within:
-	objectStores	[]*ObjectStoreIfc
+	objectStoreCollection	[]*ObjectStoreIfc		// objectStoreCollection[N] -> *ObjectStoreIfc
+	objectStoreMap		map[string]*ObjectStoreIfc	// objectStoreMap[name] -> *ObjectStoreIfc
 }
 
 // Make a new one of these!
 func NewObjectStoreManager() *ObjectStoreManager {
 	osm := ObjectStoreManager{
-		objectStores:	make([]*ObjectStoreIfc, 0),
+		objectStoreCollection:	make([]*ObjectStoreIfc, 0),
 	}
 	return &osm
 }
 
-// Add an ObjectStore to the set
+// Scan Method functionality
+// ------------------------------------------------------------------------------------------------
+
+// Add an unnamed ObjectStore to the set
+// The only way to access this ObjectStore is using the Scan Method
 // Remember: each addition is lower in priority than the previous!
 // objectStore parameter must be a pointer to a concrete implementation of an ObjectStoreIfc
 // Ref: https://stackoverflow.com/questions/24422810/golang-convert-struct-pointer-to-interface#
 func (osm *ObjectStoreManager) AddObjectStore(objectStore interface{}) error {
 	if store, ok := objectStore.(ObjectStoreIfc); ok {
 		lib.GetLogger().Trace("Adding Object ObjectStore")
-		osm.objectStores = append(osm.objectStores, &store)
+		osm.objectStoreCollection = append(osm.objectStoreCollection, &store)
 		return nil
 	}
 	return lib.GetLogger().Error("Supplied ObjectStore does not satisfy ObjectStoreIfc")
@@ -45,7 +54,7 @@ func (osm *ObjectStoreManager) AddObjectStore(objectStore interface{}) error {
 func (osm *ObjectStoreManager) GetObject(path string) *Object {
 	lib.GetLogger().Trace(fmt.Sprintf("ObjectStoreManager.GetObject('%s')", path))
 	// Scan UP the list of ObjectStores in the search for this Object by path
-	for _, store := range osm.objectStores {
+	for _, store := range osm.objectStoreCollection {
 		res := (*store).GetObject(path)
 		if nil != res { return res }
 	}
@@ -117,5 +126,54 @@ func (osm *ObjectStoreManager) FindTemplate(language string, name string) *Objec
 		name,
 	))
 	return osm.FindPrivateObject(language, fmt.Sprintf("templates/%s.mustache", name))
+}
+
+// Name Method functionality
+// ------------------------------------------------------------------------------------------------
+
+// Add an ObjectStore to the Collection and name Map with a unique name as the key
+func (osm *ObjectStoreManager) AddNamedObjectStore(name string, objectStore interface{}) error {
+	lib.GetLogger().Trace(fmt.Sprintf(
+		"ObjectStoreManager.AddNamedObjectStore('%s', ObjectStoreIfc)",
+		name,
+	))
+
+	// If we already have an ObjectStore with this name...
+	if _, ok := osm.objectStoreMap[name]; ok {
+		return lib.GetLogger().Error(fmt.Sprintf(
+			"There is already an ObjectStore in the collection with the name '%s'",
+			name,
+		))
+	}
+
+	// Try to add it to the colletion...
+	err := osm.AddObjectStore(objectStore)
+
+	// If there was no problem adding it to the collection...
+	if nil == err {
+		// And if it casts correctly...
+		if store, ok := objectStore.(ObjectStoreIfc); ok {
+			// Capture a reference to it by name into the map!
+			osm.objectStoreMap[name] = &store
+		} else {
+			return lib.GetLogger().Error("Supplied ObjectStore does not satisfy ObjectStoreIfc")
+		}
+	}
+	return err
+}
+
+// Get an Object with the specified path from named ObjectStore
+func (osm *ObjectStoreManager) GetNamedObjectStoreObject(objectStoreName string, path string) *Object {
+	lib.GetLogger().Trace(fmt.Sprintf(
+		"ObjectStoreManager.GetNamedObjectStoreObject('%s', '%s')",
+		objectStoreName, path,
+	))
+	// If we can find an ObjectStore with this name
+	if store, ok := osm.objectStoreMap[objectStoreName]; ok {
+		// Pluck out an Object from it with this path
+		res := (*store).GetObject(path)
+		return res
+	}
+	return nil
 }
 
