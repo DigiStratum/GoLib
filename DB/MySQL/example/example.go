b439ddb9efff27d83af45e54b427da8a361a7cb9
Sean Kelly <keenskelly@gmail.com>
2021-04-22 09:03:14 -0700

    WIP -streamlining mysql connection/query/example

--- DB/MySQL/example/example.go
+++ DB/MySQL/example/example.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	db "github.com/DigiStratum/GoLib/DB"
-	mysql "github.com/DigiStratum/GoLib/DB/MySQL"
-)
-
-func main() {
-	//test_append_clone()
-	test_mysql_query()
-	//test_modify_passed_struct()
-}
-
-type Todo struct {
-	//Result
-	Id	int
-	Task	string
-	Due	string
-}
-
-// Satisfies ResultIfc
-func (t Todo) ZeroClone() (mysql.ResultIfc, mysql.PropertyPointers) {
-	n := Todo{}
-	npp := mysql.PropertyPointers{ &n.Id, &n.Task, &n.Due }
-	return &n, npp
-}
-
 /*
 
 -- MySQL Test database setup for "todolist"
@@ -42,45 +15,52 @@ grant all on todolist.* to 'username'@'localhost';
 
 */
 
+import (
+	"fmt"
+	"os"
 
-func test_modify_passed_struct() {
-	t := Todo{ Id: 1, Task: "loaf", Due: "asap" }
-	todos := modify_todo(t)
-	for _, t := range todos {
-		print_todo(t)
-	}
+	db "github.com/DigiStratum/GoLib/DB"
+	mysql "github.com/DigiStratum/GoLib/DB/MySQL"
+)
+
+type Todo struct {
+	//Result
+	Id	int
+	Task	string
+	Due	string
 }
 
-func modify_todo(t Todo) []Todo {
-	todos := []Todo{}
-	todos = append(todos, t)
-	t.Id = 2
-	t.Due = "nope"
-	todos = append(todos, t)
-	return todos
+// Satisfies ResultIfc
+func (t Todo) ZeroClone() (mysql.ResultIfc, mysql.PropertyPointers) {
+	n := Todo{}
+	npp := mysql.PropertyPointers{ &n.Id, &n.Task, &n.Due }
+	return &n, npp
 }
 
-func print_todo(t Todo) {
+
+func (t Todo) Print() {
 	fmt.Printf(
 		"Todo: { \"id\": \"%d\", \"task\": \"%s\", \"due\": \"%s\" }\n",
 		t.Id, t.Task, t.Due,
 	)
 }
 
-func test_mysql_query() {
+func main() {
+	// Get a database connection
 	dsn := db.MakeDSN("username", "password", "localhost", "3306", "todolist")
 	fmt.Printf("MySQL DSN is: %s\n", dsn)
+
+/*
+	// Get the connection through a connection Manager if you want to manage multiple connections to different DB's
 	manager := mysql.NewManager()
-
 	dbKey, err := manager.Connect(dsn)
-	if nil != err {
-		fmt.Printf("Error connecting: %s\n", err.Error())
-	}
-
+	if nil != err { die(fmt.Sprintf("Error connecting: %s\n", err.Error())) }
 	dbConn, err := manager.GetConnection(*dbKey)
-	if nil != err {
-		fmt.Printf("Error getting connection: %s\n", err.Error())
-	}
+*/
+	dbConn, err := mysql.NewConnection(dsn)
+	if nil != err { die(fmt.Sprintf("Error getting connection: %s\n", err.Error())) }
+
+	// Run the query
 	query := mysql.NewQuery(
 		"SELECT id, task, due FROM todo;",
 		Todo{},
@@ -92,7 +72,7 @@ func test_mysql_query() {
 	} else {
 		for index, result := range *results {
 			if todoResult, ok := result.(*Todo); ok {
-				print_todo(*todoResult)
+				todoResult.Print()
 			} else {
 				fmt.Printf("Error converting result record to todo{%d}\n", index)
 			}
@@ -102,27 +82,8 @@ func test_mysql_query() {
 	manager.Disconnect(*dbKey)
 }
 
-type record struct {
-	value		int
-}
-
-// This test snippet demonstrates that an instance of a struct is cloned when it is appended() to a slice.
-func test_append_clone() {
-	// A proper struct
-	r := record{
-		value: 1,
-	}
-
-	collection := []record{}
-
-	collection = append(collection, r)
-	r.value++
-	collection = append(collection, r)
-	r.value++
-	collection = append(collection, r)
-
-	for index, record := range collection {
-		fmt.Printf("collection[%d].record.value = %d\n", index, record.value)
-	}
+func die(msg string) {
+	fmt.Printf("%s\n", msg)
+	os.Exit(1)
 }
 
--- DB/MySQL/query.go
+++ DB/MySQL/query.go
@@ -14,18 +14,13 @@ import (
 type Query struct {
 	query		string          // The query to execute as prepared statement
 	prototype	ResultIfc       // Object to use as a prototype to produce query Result row objects
-	//resultFactory	*ResultFactory	// Object to use as a prototype to produce query Result row objects
 }
 
 // Make a new one of these
-//func NewQuery(query string, prototype ResultIfc) *Query {
-//func NewQuery(query string, resultFactory ResultFactory) *Query {
 func NewQuery(query string, prototype ResultIfc) *Query {
-	//resultFactory := NewResultFactory(prototype)
 	q := Query{
 		query:		query,
 		prototype:	prototype,
-		//resultFactory:	resultFactory,
 	}
 	return &q
 }
@@ -45,9 +40,6 @@ func (q *Query) Run(conn *Connection, args ...interface{}) (*ResultSet, error) {
 	for rows.Next() {
 		// Make a new result object for this row
 		// (... and get pointers to all the all the result object members)
-		//result := (*q).resultPrototype.GetZeroClone()
-		//result, resultProperties, err := (*q).resultFactory.MakeNewResult()
-		//if nil != err { return nil, err }
 		result, resultProperties := (*q).prototype.ZeroClone()
 
 		// Read MySQL columns for this row into the result object member pointers
@@ -60,106 +52,3 @@ func (q *Query) Run(conn *Connection, args ...interface{}) (*ResultSet, error) {
 	return &results, nil
 }
 
-
-
-/*
-// ------------------------------------------------------------------------------------------------
-// Query Bits
-
-// ------------------------------------------------------------------------------------------------
-// Execute a query with varargs for substitution and Structured results
-// ref: https://appliedgo.net/generics/
-// ref: https://stackoverflow.com/questions/37851500/how-to-copy-an-interface-value-in-go
-// ref: https://forum.golangbridge.org/t/database-rows-scan-unknown-number-of-columns-json/7378/2
-// ref: https://stackoverflow.com/questions/26744873/converting-map-to-struct/26746461
-// ref: https://stackoverflow.com/questions/29184933/golang-reflect-get-pointer-to-a-struct-field-value
-func (dbc *Connection) Query(querySpec *QuerySpec, args ...interface{}) (*[]interface{}, error) {
-	results := []interface{}{}
-	//template := querySpec.Template
-	template := querySpec.ResultFactory()
-
-	// Ref: https://stackoverflow.com/questions/18926303/iterate-through-the-fields-of-a-struct-in-go
-	numFields := reflect.TypeOf(template).NumField()
-	fmt.Printf("SQuery() QuerySpec.Template has %d Fields\n", numFields)
-	values := make([]interface{}, numFields)
-	templateValue := reflect.ValueOf(template)
-	tvType := templateValue.Type()
-	//templateValue := reflect.ValueOf(querySpec.Template).Elem()
-	for i := 0; i < numFields; i++ {
-		//fi := templateValue.Field(i).Interface()
-		//values[i] = &fi
-		// ref: https://stackoverflow.com/questions/27992821/how-get-pointer-of-structs-member-from-interface
-		//values[i] = templateValue.Field(i).Addr().Interface()
-		//values[i] = templateValue.Elem().FieldByIndex(i).Addr().Interface()
-		//values[i] = templateValue.Field(i).Addr()
-		//values[i] = templateValue.Field(i).Interface().Addr()
-		//values[i] = templateValue.Field(i).Interface()
-
-		// ref: https://stackoverflow.com/questions/29184933/golang-reflect-get-pointer-to-a-struct-field-value
-		//valueField := templateValue.Field(i)
-		//values[i] = valueField.Addr().Interface()
-
-		// ref: https://samwize.com/2015/03/20/how-to-use-reflect-to-set-a-struct-field/
-		fieldName := tvType.Field(i).Name
-		field:= templateValue.Field(i)
-		fmt.Printf("Field name: '%s', type: '%s'\n", fieldName, field.Type())
-		//v := templateValue.FieldByName(fieldName).Interface()
-		v := field.Interface()
-		switch field.Type().String() {
-			case "*int":
-				values[i] = v.(*int)
-			case "*string":
-				values[i] = v.(*string)
-		}
-		//values[i] = templateValue.Elem().FieldByName(fieldName).Addr().Interface()
-	}
-
-	protoQuery := querySpec.Query
-	// TODO: expand querySpec.Query '???' placeholders
-	finalQuery := protoQuery
-
-	// Convert string args to interface{} for Query()
-	//iArgs := make([]interface{}, len(args))
-	//for i, v := range args { iArgs[i] = v }
-
-	// Execute the Query
-	//rows, err := dbc.Conn.Query(finalQuery, iArgs...)
-	rows, err := dbc.Conn.Query(finalQuery, args...)
-	if err != nil { return nil, err }
-
-	// Process the result rows
-	for rows.Next() {
-
-		// figure out what columns were returned
-		// the column names will be the JSON object field keys
-		//columns, err := rows.ColumnTypes()
-		//if err != nil { return nil, err }
-
-		// Scan needs an array of pointers to the values it is setting
-		// This creates the object and sets the values correctly
-		//values := make([]interface{}, len(columns))
-		//object := map[string]interface{}{}
-		//for i, column := range columns {
-		//	object[column.Name()] = reflect.New(column.ScanType()).Interface()
-		//	values[i] = object[column.Name()]
-		//}
-		//err = rows.Scan(values...)
-		//if err != nil { return nil, err }
-		//results = append(results, template)
-		// Make a new result object for this row
-		result, err := querySpec.ResultFactory.NewResult()
-		if nil != err { return nil, err }
-
-		// Get pointers to all the all the result object members
-		resultProperties := result.GetPropertyPointers()
-
-		// Read MySQL columns for this row into the result object member pointers
-		err = rows.Scan(resultProperties...)
-		if nil != err { return nil, err }
-
-		results = append(results, result)
-	}
-
-	return &results, nil
-}
-*/
