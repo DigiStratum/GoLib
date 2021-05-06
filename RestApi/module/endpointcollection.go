3983f762034a8b8ae856f25b9c6d76730f036b8c
Sean Kelly <keenskelly@gmail.com>
2021-05-05 23:04:48 -0700

    Update code comments

--- RestApi/module/endpointcollection.go
+++ RestApi/module/endpointcollection.go
@@ -1,18 +1,10 @@
 package module
 
 /*
-Collection for Endpoints
+Collection of Endpoints
 
-This is a simple mechanism for Endpoints to be able to automatically register themselves from their
-own init() functions without having to be called to do so. It reduces boilerplate code, thus making
-the programming interface a simpler one to understand.
-
-This is implemented as a singleton which is globally accessible across all Modules. The functional
-design is such that, because each Module initializes one at a time, in sequence, at the time of a
-given Module's load as a plugin, the init() functions for all the Endpoints found can register here,
-then, when the Module initializes its own Controller, the Controller can pull the Endpoints out of
-here. When the Endpoints are pulled out by the Controller, the Collection is once again empty, ready
-for the next Module.
+Each Endpoint of a given Module must add itself to this collection with the AddEndpiont() function.
+Use Module.GetEndpointCollectionInstance() to get the singleton instance and then call AddEndpoint().
 */
 
 // Collection public interface
--- RestApi/module/module.go
+++ RestApi/module/module.go
@@ -1,5 +1,14 @@
 package module
 
+/*
+This is the integration contract for a Module implementation. From the Stratify SaaS Platform
+perspective, a Module is the middle layer of HTTP concerns. It is responsible for receiving and
+delegating the handling of HTTP Requests to Endpoints that are mapped to it through configuration.
+
+Any Module implementation MUST implement the Required Module public interface. A Module MAY also
+implement any of the Optional Module public interfaces as needed.
+*/
+
 import(
 	lib "github.com/DigiStratum/GoLib"
 )
