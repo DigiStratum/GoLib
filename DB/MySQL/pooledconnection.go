b35f3256c5bd9f49af26d855c7cbf28325e4e5f1
Sean Kelly <keenskelly@gmail.com>
2021-07-31 10:54:00 -0700

    Update pooled connection last touch times for real actions
    Not every action is meaningful work being done with the database connection. We don't really want to keep connections with active leases being held down if they are not in active use.

--- DB/MySQL/pooledconnection.go
+++ DB/MySQL/pooledconnection.go
@@ -72,9 +72,9 @@ func NewPooledConnection(dsn string, connPool ConnectionPoolIfc) (PooledConnecti
 
 // Connections
 func (pc *pooledConnection) IsConnected() bool { return (*pc).connection.IsConnected() }
-func (pc *pooledConnection) Connect() error { return (*pc).connection.Connect() }
+func (pc *pooledConnection) Connect() error { pc.Touch(); return (*pc).connection.Connect() }
 func (pc *pooledConnection) Disconnect() { (*pc).connection.Disconnect() }
-func (pc *pooledConnection) Reconnect() { (*pc).connection.Reconnect() }
+func (pc *pooledConnection) Reconnect() { pc.Touch(); (*pc).connection.Reconnect() }
 
 // Leasing
 func (pc *pooledConnection) IsLeased() bool { return (*pc).isLeased }
@@ -108,16 +108,16 @@ func (pc *pooledConnection) Touch() {
 func (pc *pooledConnection) InTransaction() bool { return (*pc).connection.InTransaction() }
 func (pc *pooledConnection) Rollback() error { return (*pc).connection.Rollback() }
 func (pc *pooledConnection) Begin() error { return (*pc).connection.Begin() }
-func (pc *pooledConnection) Commit() error { return (*pc).connection.Commit() }
+func (pc *pooledConnection) Commit() error { pc.Touch(); return (*pc).connection.Commit() }
 
 // Operations
 func (pc *pooledConnection) Prepare(query string) (*db.Stmt, error) { return (*pc).connection.Prepare(query) }
-func (pc *pooledConnection) Exec(query string, args ...interface{}) (db.Result, error) { return (*pc).connection.Exec(query, args...) }
-func (pc *pooledConnection) Query(query string, args ...interface{}) (*db.Rows, error) { return (*pc).connection.Query(query, args...) }
-func (pc *pooledConnection) QueryRow(query string, args ...interface{}) *db.Row { return (*pc).connection.QueryRow(query, args...) }
+func (pc *pooledConnection) Exec(query string, args ...interface{}) (db.Result, error) { pc.Touch(); return (*pc).connection.Exec(query, args...) }
+func (pc *pooledConnection) Query(query string, args ...interface{}) (*db.Rows, error) { pc.Touch(); return (*pc).connection.Query(query, args...) }
+func (pc *pooledConnection) QueryRow(query string, args ...interface{}) *db.Row { pc.Touch(); return (*pc).connection.QueryRow(query, args...) }
 
 
 // Statements
-func (pc *pooledConnection) StmtExec(stmt *db.Stmt, args ...interface{}) (db.Result, error) { return (*pc).connection.StmtExec(stmt, args...) }
-func (pc *pooledConnection) StmtQuery(stmt *db.Stmt, args ...interface{}) (*db.Rows, error) {  return (*pc).connection.StmtQuery(stmt, args...) }
-func (pc *pooledConnection) StmtQueryRow(stmt *db.Stmt, args ...interface{}) *db.Row {  return (*pc).connection.StmtQueryRow(stmt, args...) }
+func (pc *pooledConnection) StmtExec(stmt *db.Stmt, args ...interface{}) (db.Result, error) { pc.Touch(); return (*pc).connection.StmtExec(stmt, args...) }
+func (pc *pooledConnection) StmtQuery(stmt *db.Stmt, args ...interface{}) (*db.Rows, error) {  pc.Touch(); return (*pc).connection.StmtQuery(stmt, args...) }
+func (pc *pooledConnection) StmtQueryRow(stmt *db.Stmt, args ...interface{}) *db.Row {  pc.Touch(); return (*pc).connection.StmtQueryRow(stmt, args...) }
