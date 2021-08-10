03cd939de52e5cf96e4d81544106a3db0b1ae450
Sean Kelly <keenskelly@gmail.com>
2021-08-09 22:46:49 -0700

    WIP - Refactor Nullables

--- DB/MySQL/nullables/nullables.go
+++ DB/MySQL/nullables/nullables.go
@@ -85,7 +85,7 @@ type NullableIfc interface {
         Scan(value interface{}) error
 }
 
-type nullable struct {
+type Nullable struct {
 	isNil		bool
 	nullableType	NullableType
 	ni		NullInt64
@@ -96,8 +96,8 @@ type nullable struct {
 }
 
 // Make a new one of these!
-func NewNullable(value interface{}) NullableIfc {
-	n := nullable{
+func NewNullable(value interface{}) *Nullable {
+	n := Nullable{
 		isNil:		true,
 		nullableType:	NULLABLE_NIL,
 		ni:		NullInt64{ Valid: false },
@@ -114,114 +114,114 @@ func NewNullable(value interface{}) NullableIfc {
 // NullableIfc Public Interface
 // -------------------------------------------------------------------------------------------------
 
-func (n *nullable) IsNil() bool { return (*n).isNil }
+func (r Nullable) IsNil() bool { return r.isNil }
 
 // Convert value to appropriate Nullable; return true on success, else false
-func (n *nullable) SetValue(value interface{}) bool {
+func (r *Nullable) SetValue(value interface{}) bool {
 	if v, ok := value.(int64); ok {
-		n.setInt64(v)
+		r.setInt64(v)
 	} else if v, ok := value.(bool); ok {
-		n.setBool(v)
+		r.setBool(v)
 	} else if v, ok := value.(float64); ok {
-		n.setFloat64(v)
+		r.setFloat64(v)
 	} else if v, ok := value.(string); ok {
-		n.setString(v)
+		r.setString(v)
 	} else if v, ok := value.(time.Time); ok {
-		n.setTime(v)
+		r.setTime(v)
 	} else { return false }
 	return true
 }
 
-func (n *nullable) setInt64(value int64) {
-	(*n).nullableType = NULLABLE_INT64
-	(*n).ni.Int64 = value
-	(*n).ni.Valid = true
-	(*n).isNil = false
+func (r *Nullable) setInt64(value int64) {
+	r.nullableType = NULLABLE_INT64
+	r.ni.Int64 = value
+	r.ni.Valid = true
+	r.isNil = false
 }
 
-func (n *nullable) setBool(value bool) {
-	(*n).nullableType = NULLABLE_BOOL
-	(*n).nb.Bool = value
-	(*n).nb.Valid = true
-	(*n).isNil = false
+func (r *Nullable) setBool(value bool) {
+	r.nullableType = NULLABLE_BOOL
+	r.nb.Bool = value
+	r.nb.Valid = true
+	r.isNil = false
 }
 
-func (n *nullable) setFloat64(value float64) {
-	(*n).nullableType = NULLABLE_FLOAT64
-	(*n).nf.Float64 = value
-	(*n).nf.Valid = true
-	(*n).isNil = false
+func (r *Nullable) setFloat64(value float64) {
+	r.nullableType = NULLABLE_FLOAT64
+	r.nf.Float64 = value
+	r.nf.Valid = true
+	r.isNil = false
 }
 
-func (n *nullable) setString(value string) {
-	(*n).nullableType = NULLABLE_STRING
-	(*n).ns.String = value
-	(*n).ns.Valid = true
-	(*n).isNil = false
+func (r *Nullable) setString(value string) {
+	r.nullableType = NULLABLE_STRING
+	r.ns.String = value
+	r.ns.Valid = true
+	r.isNil = false
 }
 
-func (n *nullable) setTime(value time.Time) {
-	(*n).nullableType = NULLABLE_TIME
-	(*n).nt.Time = value
-	(*n).nt.Valid = true
-	(*n).isNil = false
+func (r *Nullable) setTime(value time.Time) {
+	r.nullableType = NULLABLE_TIME
+	r.nt.Time = value
+	r.nt.Valid = true
+	r.isNil = false
 }
 
-func (n *nullable) GetType() NullableType { return (*n).nullableType }
+func (r *Nullable) GetType() NullableType { return r.nullableType }
 
-func (n *nullable) IsInt64() bool { return (*n).nullableType == NULLABLE_NIL }
-func (n *nullable) IsBool() bool { return (*n).nullableType == NULLABLE_BOOL }
-func (n *nullable) IsFloat64() bool { return (*n).nullableType == NULLABLE_FLOAT64 }
-func (n *nullable) IsString() bool { return (*n).nullableType == NULLABLE_STRING }
-func (n *nullable) IsTime() bool { return (*n).nullableType == NULLABLE_TIME }
+func (r *Nullable) IsInt64() bool { return r.nullableType == NULLABLE_NIL }
+func (r *Nullable) IsBool() bool { return r.nullableType == NULLABLE_BOOL }
+func (r *Nullable) IsFloat64() bool { return r.nullableType == NULLABLE_FLOAT64 }
+func (r *Nullable) IsString() bool { return r.nullableType == NULLABLE_STRING }
+func (r *Nullable) IsTime() bool { return r.nullableType == NULLABLE_TIME }
 
 // Return the value as an Int64, complete with data conversions, or nil if nil or conversion problem
-func (n *nullable) GetInt64() *int64 {
-	if n.IsNil() { return nil }
+func (r Nullable) GetInt64() *int64 {
+	if r.IsNil() { return nil }
 
 	// NullInt64 passes through unmodified
-	if (*n).ni.Valid { return &(*n).ni.Int64 }
+	if r.ni.Valid { return &r.ni.Int64 }
 
 	// NullBool converts to a int64
-	if (*n).nb.Valid {
+	if r.nb.Valid {
 		var v int64 = 0
-		if (*n).nb.Bool { v = 1 }
+		if r.nb.Bool { v = 1 }
 		return &v
 	}
 
 	// NullFloat64 converts to an int64
-	if (*n).nf.Valid { v := int64((*n).nf.Float64); return &v }
+	if r.nf.Valid { v := int64(r.nf.Float64); return &v }
 
 	// NullString converts to an int64
-	if (*n).ns.Valid {
-		if vc, err := strconv.ParseInt((*n).ns.String, 0, 64); nil == err { return &vc }
+	if r.ns.Valid {
+		if vc, err := strconv.ParseInt(r.ns.String, 0, 64); nil == err { return &vc }
 		return nil
 	}
 
 	// NullTime converts to an int64 (timestamp)
-	if (*n).nt.Valid { vc := (*n).nt.Time.Unix(); return &vc }
+	if r.nt.Valid { vc := r.nt.Time.Unix(); return &vc }
 
 	return nil
 }
 
 // Return the value as a bool, complete with data conversions, or nil if nil or conversion problem
-func (n *nullable) GetBool() *bool {
-	if n.IsNil() { return nil }
+func (r Nullable) GetBool() *bool {
+	if r.IsNil() { return nil }
 
 	// NullInt64 converts to a bool
-	if (*n).ni.Valid { v := ((*n).ni.Int64 == 0); return &v }
+	if r.ni.Valid { v := (r.ni.Int64 == 0); return &v }
 
 	// NullBool passes through unmodified
-	if (*n).nb.Valid { return &(*n).nb.Bool }
+	if r.nb.Valid { return &r.nb.Bool }
 
 	// NullFloat64 converts to a bool (true if we drop the decimal and the remaining int != 0)
-	if (*n).nf.Valid { v := (int64((*n).nf.Float64) != 0); return &v }
+	if r.nf.Valid { v := (int64(r.nf.Float64) != 0); return &v }
 
 	// NullString converts to a bool (true if "true" or stringified int and != 0 )
-	if (*n).ns.Valid {
-		lcv := strings.ToLower((*n).ns.String)
+	if r.ns.Valid {
+		lcv := strings.ToLower(r.ns.String)
 		if lcv == "true" { v := true; return &v }
-		if vc, err := strconv.ParseInt((*n).ns.String, 0, 64); nil != err {
+		if vc, err := strconv.ParseInt(r.ns.String, 0, 64); nil != err {
 			v := (vc != 0);
 			return &v
 		}
@@ -229,74 +229,74 @@ func (n *nullable) GetBool() *bool {
 	}
 
 	// NullTime converts to a bool (true if non-null)
-	if (*n).nt.Valid { v := true; return &v }
+	if r.nt.Valid { v := true; return &v }
 
 	return nil
 }
 
 // Return the value as a Float64, complete with data conversions, or nil if nil or conversion problem
-func (n *nullable) GetFloat64() *float64 {
-	if n.IsNil() { return nil }
+func (r Nullable) GetFloat64() *float64 {
+	if r.IsNil() { return nil }
 
 	// NullInt64 converts to a Float64
-	if (*n).ni.Valid { vc := float64((*n).ni.Int64); return &vc }
+	if r.ni.Valid { vc := float64(r.ni.Int64); return &vc }
 
 	// NullBool converts to a Float64
 	// we use 2.0|0.0 for true|false, respectively so that inverse conversion works.
 	// Precision rounding reduces 1.0 to < 1 (0.999) which when converted back would yield 0 decimal value (false)
-	if (*n).nb.Valid {
+	if r.nb.Valid {
 		var v float64 = 0.0
-		if (*n).nb.Bool { v = 2.0 }
+		if r.nb.Bool { v = 2.0 }
 		return &v
 	}
 
 	// NullFloat64 passes through unmodified
-	if (*n).nf.Valid { return &(*n).nf.Float64 }
+	if r.nf.Valid { return &r.nf.Float64 }
 
 	// NullString converts to a Float64
-	if (*n).ns.Valid {
-		if vc, err := strconv.ParseFloat((*n).ns.String, 64); nil != err { return &vc }
+	if r.ns.Valid {
+		if vc, err := strconv.ParseFloat(r.ns.String, 64); nil != err { return &vc }
 		return nil
 	}
 
 	// NullTime conversion to a Float64 not supported
 	// (timestamp) would lose precision, so we will 0 it out, no value
-	if (*n).nt.Valid { return nil }
+	if r.nt.Valid { return nil }
 
 	return nil
 }
 
 // Return the value as a *string, complete with data conversions, or nil if nil or conversion problem
-func (n *nullable) GetString() *string {
-	if n.IsNil() { return nil }
+func (r Nullable) GetString() *string {
+	if r.IsNil() { return nil }
 
 	// NullInt64 converts to a string
-	if (*n).ni.Valid {
-		v := fmt.Sprintf("%d", (*n).ni.Int64)
+	if r.ni.Valid {
+		v := fmt.Sprintf("%d", r.ni.Int64)
 		return &v
 	}
 
 	// NullBool converts to a string
-	if (*n).nb.Valid {
+	if r.nb.Valid {
 		v := "false"
-		if (*n).nb.Bool { v = "true" }
+		if r.nb.Bool { v = "true" }
 		return &v
 	}
 
 	// NullFloat64 converts to a string
-	if (*n).nf.Valid {
-		v := strconv.FormatFloat((*n).nf.Float64, 'E', -1, 64)
+	if r.nf.Valid {
+		v := strconv.FormatFloat(r.nf.Float64, 'E', -1, 64)
 		return &v
 	}
 
 	// NullString passes through unmodified
-	if (*n).ns.Valid { return &(*n).ns.String }
+	if r.ns.Valid { return &r.ns.String }
 
 	// NullTime converts to a string
 	// ref: https://stackoverflow.com/questions/33119748/convert-time-time-to-string
 	// ref: (so annoying...) https://pkg.go.dev/time#Time.Format
-	if (*n).nt.Valid {
-		v := (*n).nt.Time.Format("2006-01-02T15:04:05Z")
+	if r.nt.Valid {
+		v := r.nt.Time.Format("2006-01-02T15:04:05Z")
 		return &v
 	}
 
@@ -304,69 +304,69 @@ func (n *nullable) GetString() *string {
 }
 
 // Return the value as a *time.Time, complete with data conversions, or nil if nil or conversion problem
-func (n *nullable) GetTime() *time.Time {
-	if n.IsNil() { return nil }
+func (r Nullable) GetTime() *time.Time {
+	if r.IsNil() { return nil }
 
 	// NullInt64 converts to a time.Time (unix timestamp)
-	if (*n).ni.Valid {
-		v := time.Unix((*n).ni.Int64, 0)
+	if r.ni.Valid {
+		v := time.Unix(r.ni.Int64, 0)
 		return &v
 	}
 
 	// NullBool does not convert...
-	if (*n).nb.Valid { return nil }
+	if r.nb.Valid { return nil }
 
 	// NullFloat64 converts to an int64, then to a time
-	if (*n).nf.Valid {
-		v := time.Unix(int64((*n).nf.Float64), 0)
+	if r.nf.Valid {
+		v := time.Unix(int64(r.nf.Float64), 0)
 		return &v
 	}
 
 	// NullString parses as a datetime (MySQL style)
-	if (*n).ns.Valid {
-		v, err := time.Parse("2006-01-02T15:04:05Z", (*n).ns.String)
+	if r.ns.Valid {
+		v, err := time.Parse("2006-01-02T15:04:05Z", r.ns.String)
 		if nil != err { return nil }
 		return &v
 	}
 
 	// NullTime passes through unmodified
-	if (*n).nt.Valid { return &(*n).nt.Time	}
+	if r.nt.Valid { return &r.nt.Time	}
 
 	return nil
 }
 
 // MarshalJSON for Nullable - we just sub it out to the underlying Nullable type
-func (n *nullable) MarshalJSON() ([]byte, error) {
-	switch (*n).nullableType {
-		case NULLABLE_INT64: return (*n).ni.MarshalJSON()
-		case NULLABLE_BOOL: return (*n).nb.MarshalJSON()
-		case NULLABLE_FLOAT64: return (*n).nf.MarshalJSON()
-		case NULLABLE_STRING: return (*n).ns.MarshalJSON()
-		case NULLABLE_TIME: return (*n).nt.MarshalJSON()
+func (r Nullable) MarshalJSON() ([]byte, error) {
+	switch r.nullableType {
+		case NULLABLE_INT64: return r.ni.MarshalJSON()
+		case NULLABLE_BOOL: return r.nb.MarshalJSON()
+		case NULLABLE_FLOAT64: return r.nf.MarshalJSON()
+		case NULLABLE_STRING: return r.ns.MarshalJSON()
+		case NULLABLE_TIME: return r.nt.MarshalJSON()
 		default: return []byte("null"), nil
 	}
 }
 
 // UnmarshalJSON for Nullable - we just sub it out to the underlying Nullable type
-func (n *nullable) UnmarshalJSON(b []byte) error {
-	switch (*n).nullableType {
-		case NULLABLE_INT64: return (*n).ni.UnmarshalJSON(b)
-		case NULLABLE_BOOL: return (*n).nb.UnmarshalJSON(b)
-		case NULLABLE_FLOAT64: return (*n).nf.UnmarshalJSON(b)
-		case NULLABLE_STRING: return (*n).ns.UnmarshalJSON(b)
-		case NULLABLE_TIME: return (*n).nt.UnmarshalJSON(b)
+func (r *Nullable) UnmarshalJSON(b []byte) error {
+	switch r.nullableType {
+		case NULLABLE_INT64: return r.ni.UnmarshalJSON(b)
+		case NULLABLE_BOOL: return r.nb.UnmarshalJSON(b)
+		case NULLABLE_FLOAT64: return r.nf.UnmarshalJSON(b)
+		case NULLABLE_STRING: return r.ns.UnmarshalJSON(b)
+		case NULLABLE_TIME: return r.nt.UnmarshalJSON(b)
 		default: return nil
 	}
 }
 
 // Scan for Nullable - we just sub it out to the underlying Nullable type
-func (n *nullable) Scan(value interface{}) error {
-	switch (*n).nullableType {
-		case NULLABLE_INT64: return (*n).ni.Scan(value)
-		case NULLABLE_BOOL: return (*n).nb.Scan(value)
-		case NULLABLE_FLOAT64: return (*n).nf.Scan(value)
-		case NULLABLE_STRING: return (*n).ns.Scan(value)
-		case NULLABLE_TIME: return (*n).nt.Scan(value)
+func (r *Nullable) Scan(value interface{}) error {
+	switch r.nullableType {
+		case NULLABLE_INT64: return r.ni.Scan(value)
+		case NULLABLE_BOOL: return r.nb.Scan(value)
+		case NULLABLE_FLOAT64: return r.nf.Scan(value)
+		case NULLABLE_STRING: return r.ns.Scan(value)
+		case NULLABLE_TIME: return r.nt.Scan(value)
 		default: return errors.New("Unsupported Nullable Type (oversight in implementation!)")
 	}
 }
