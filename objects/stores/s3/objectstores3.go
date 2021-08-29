faf2206f6a81d69693edaea54c621c5aed4c06c6
Sean Kelly <keenskelly@gmail.com>
2021-08-28 22:02:09 -0700

    WIP - Refarctoring - ObjectStoreS3

--- objects/stores/s3/objectstores3.go
+++ objects/stores/s3/objectstores3.go
@@ -1,4 +1,4 @@
-package objects
+package s3
 
 /*
 
@@ -35,6 +35,9 @@ import (
 	"github.com/DigiStratum/GoLib/Cloud"
 )
 
+type ObjectStoreS3Ifc interface {
+}
+
 type ObjectStoreS3 struct {
 	storeConfig	lib.ConfigIfc
 	awsS3		*s3.S3
@@ -42,6 +45,10 @@ type ObjectStoreS3 struct {
 	readCache	*MutableObjectStore
 	awsHelper	*cloud.AWSHelper
 }
+
+// -------------------------------------------------------------------------------------------------
+// Factory Functions
+// -------------------------------------------------------------------------------------------------
 
 // Make a new one of these!
 func NewObjectStoreS3() *ObjectStoreS3 {
@@ -51,32 +58,34 @@ func NewObjectStoreS3() *ObjectStoreS3 {
 	return &r
 }
 
-// Satisfies RespositoryIfc
-func (os *ObjectStoreS3) Configure(config lib.ConfigIfc) error {
+// -------------------------------------------------------------------------------------------------
+// ObjectStoreIfc Public Interface
+// -------------------------------------------------------------------------------------------------
+
+func (r *ObjectStoreS3) Configure(config lib.ConfigIfc) error {
 
 	// Validate that the config has what we need for S3!
 	requiredConfig := []string{ "awsregion", "s3bucket", "s3folder" }
 	if ! (config.HasAll(&requiredConfig)) {
 		return errors.New("Incomplete ObjectStoreS3 configuration provided")
 	}
-	os.storeConfig = config
+	r.storeConfig = config
 
 	// Light up our AWS Helper with the region from our configuration data
-	os.awsHelper = cloud.NewAWSHelper(config.Get("awsregion"))
+	r.awsHelper = cloud.NewAWSHelper(config.Get("awsregion"))
 	return nil
 }
 
-// Satisfies ObjectStoreIfc
 // Ref: https://stackoverflow.com/questions/41645377/golang-s3-download-to-buffer-using-s3manager-downloader
-func (os *ObjectStoreS3) GetObject(path string) *Object {
+func (r *ObjectStoreS3) GetObject(path string) *Object {
 	// If it's not yet in the cache
-	if ! os.readCache.HasObject(path) {
+	if ! r.readCache.HasObject(path) {
 		// Read the Object from our S3 bucket into cache
 		buff := &aws.WriteAtBuffer{}
-		downloader := os.getS3Downloader()
+		downloader := r.getS3Downloader()
 
 		// The S3 key is the path prefixed with our configured folder for this store, if any
-		s3Folder := os.storeConfig.Get("s3folder")
+		s3Folder := r.storeConfig.Get("s3folder")
 		key := path
 		if len(s3Folder) > 0 { key = fmt.Sprintf("%s/%s", s3Folder, path) }
 
@@ -93,50 +102,56 @@ func (os *ObjectStoreS3) GetObject(path string) *Object {
 			lib.GetLogger().Error(fmt.Sprintf("ObjectStoreS3.GetObject(%s) Error : '%s'", path, err.Error()))
 			return nil
 		}
-		os.readCache.PutObject(path, NewObjectFromString(string(buff.Bytes())))
+		r.readCache.PutObject(path, NewObjectFromString(string(buff.Bytes())))
 	}
-	return os.readCache.GetObject(path)
+	return r.readCache.GetObject(path)
 }
 
 // Satisfies ObjectStoreIfc
-func (os *ObjectStoreS3) HasObject(path string) bool {
+func (r ObjectStoreS3) HasObject(path string) bool {
 	// If it's already in the cache, then we know we have it!
-	if os.readCache.HasObject(path) { return true }
+	if r.readCache.HasObject(path) { return true }
 
 	// If there's S3 metadata with no error, then there's a Object!
 	// ref: github.com/aws/aws-sdk-go/service/s3/examples_test.go ("HeadObject")
-	awsS3 := os.getS3()
+	awsS3 := r.getS3()
 	_, err := awsS3.HeadObject(&s3.HeadObjectInput{
-			Bucket: aws.String(os.storeConfig.Get("s3bucket")),
-			Key:	aws.String(os.storeConfig.Get("s3folder") + "/" + path),
+			Bucket: aws.String(r.storeConfig.Get("s3bucket")),
+			Key:	aws.String(r.storeConfig.Get("s3folder") + "/" + path),
 		},
 	)
 	return nil == err
 }
 
-// Satisfies MutableObjectStoreIfc
-func (os *ObjectStoreS3) PutObject(path string, object *Object) error {
+// -------------------------------------------------------------------------------------------------
+// MutableObjectStoreIfc Public Interface
+// -------------------------------------------------------------------------------------------------
+
+func (r *ObjectStoreS3) PutObject(path string, object *Object) error {
 	// TODO: Actually implement WRITE operation to S3 here
 	return errors.New("Not Yet Implemented!")
 }
 
+// -------------------------------------------------------------------------------------------------
+// ObjectStoreS3 Private Interface
+// -------------------------------------------------------------------------------------------------
+
 // Get our S3 connection
-func (os *ObjectStoreS3) getS3() *s3.S3 {
-	if nil == os.awsS3 {
-		sess := os.awsHelper.GetSession();
+func (r ObjectStoreS3) getS3() *s3.S3 {
+	if nil == r.awsS3 {
+		sess := r.awsHelper.GetSession();
 		if nil == sess { return nil }
-		os.awsS3 = s3.New(sess)
+		r.awsS3 = s3.New(sess)
 	}
-	return os.awsS3
+	return r.awsS3
 }
 
 // Get our S3 Downloader
-func (os *ObjectStoreS3) getS3Downloader() *s3manager.Downloader {
-	if nil == os.awsS3Downloader {
-		sess := os.awsHelper.GetSession();
+func (r ObjectStoreS3) getS3Downloader() *s3manager.Downloader {
+	if nil == r.awsS3Downloader {
+		sess := r.awsHelper.GetSession();
 		if nil == sess { return nil }
-		os.awsS3Downloader = s3manager.NewDownloader(sess)
+		r.awsS3Downloader = s3manager.NewDownloader(sess)
 	}
-	return os.awsS3Downloader
+	return r.awsS3Downloader
 }
-
