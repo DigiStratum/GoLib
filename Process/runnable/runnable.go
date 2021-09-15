dea641106dfb614e66098f858417443bc7887400
Sean Kelly <keenskelly@gmail.com>
2021-09-14 23:20:55 -0700

    WIP - Refactoring - Fix Runnable interface

--- Process/runnable/runnable.go
+++ Process/runnable/runnable.go
@@ -5,7 +5,7 @@ A standardized interface for runnable background worker threads
 */
 
 type RunnableIfc interface {
-	func Run()
-	func IsRunning()
-	func Stop()
+	Run()
+	IsRunning() bool
+	Stop()
 }
