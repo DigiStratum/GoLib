61d3f4c2d522b1117572279aea6b9f2e0fd61aa4
Sean Kelly <keenskelly@gmail.com>
2021-09-29 22:42:48 -0700

    WIP - Refactoring - Cloud Test Coverage

--- Cloud/aws/awshelper_test.go
+++ Cloud/aws/awshelper_test.go
@@ -11,6 +11,8 @@ import(
 	"testing"
 
 	. "github.com/DigiStratum/GoLib/Testing"
+
+	cfg "github.com/DigiStratum/GoLib/Config"
 )
 
 func TestThat_AWSHelper_NewAWSHelper_ReturnsSomething(t *testing.T) {
@@ -19,6 +21,37 @@ func TestThat_AWSHelper_NewAWSHelper_ReturnsSomething(t *testing.T) {
 
 	// Verify
 	ExpectNonNil(sut, t)
+}
+
+func TestThat_AWSHelper_ImplementsConfigurableIfc(t *testing.T) {
+	// Setup
+	var sut interface{} = NewAWSHelper()
+
+	// Test
+	_, ok := sut.(cfg.ConfigurableIfc)
+
+	// Verify
+	ExpectTrue(ok, t)
+}
+
+func TestThat_AWSHelper_Configure_AppliesConfigSettings(t *testing.T) {
+	// Setup
+	sut := NewAWSHelper()
+	config := cfg.NewConfig()
+	config.Set("awsRegion", "awsRegion")
+	config.Set("awsAccessKeyId", "awsAccessKeyId")
+	config.Set("awsSecretAccessKeyId", "awsSecretAccessKeyId")
+	config.Set("awsSessionToken", "awsSessionToken")
+
+	// Test
+	err := sut.Configure(config)
+
+	// Verify
+	ExpectNil(err, t)
+	ExpectString("awsRegion", sut.awsRegion, t)
+	ExpectString("awsAccessKeyId", sut.awsAccessKeyId, t)
+	ExpectString("awsSecretAccessKeyId", sut.awsSecretAccessKeyId, t)
+	ExpectString("awsSessionToken", sut.awsSessionToken, t)
 }
 
 func TestThat_AWSHelper_GetSession_ReturnsSessionNoError(t *testing.T) {
