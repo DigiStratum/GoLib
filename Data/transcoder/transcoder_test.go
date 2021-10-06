9109c58cbcbfc336600743088ad4a344e3c275db
Sean Kelly <keenskelly@gmail.com>
2021-10-05 21:40:51 -0700

    WIP - Refactoring - Data - Transcoder - Test coverage

--- Data/transcoder/transcoder_test.go
+++ Data/transcoder/transcoder_test.go
@@ -32,6 +32,23 @@ func TestThat_Transcoder_FromString_SetterRetainsProperties_WithoutErrorResult(t
 	ExpectString(expected, string(actual), t)
 }
 
+func TestThat_Transcoder_FromString_SetterReplacesProperties_WithoutErrorResult(t *testing.T) {
+	// Setup
+	sut := NewTranscoder()
+	expected := "Vegetable Soup!"
+
+	// Testing
+	sut.FromString(&expected, ES_NONE)
+	res := sut.FromString(&expected, ES_UNKNOWN)
+
+	// Verify
+	ExpectNil(res, t)
+	ExpectInt(1, len(sut.content), t)
+	actual, ok := sut.content[ES_UNKNOWN]
+	ExpectTrue(ok, t)
+	ExpectString(expected, string(actual), t)
+}
+
 func TestThat_Transcoder_FromBytes_SetterRetainsProperties_WithoutErrorResult(t *testing.T) {
 	// Setup
 	sut := NewTranscoder()
@@ -49,6 +66,24 @@ func TestThat_Transcoder_FromBytes_SetterRetainsProperties_WithoutErrorResult(t 
 	ExpectString(expected, string(actual), t)
 }
 
+func TestThat_Transcoder_FromBytes_SetterReplacesProperties_WithoutErrorResult(t *testing.T) {
+	// Setup
+	sut := NewTranscoder()
+	expected := "Vegetable Soup!"
+	expectedBytes := []byte(expected)
+
+	// Testing
+	sut.FromBytes(&expectedBytes, ES_NONE)
+	res := sut.FromBytes(&expectedBytes, ES_UNKNOWN)
+
+	// Verify
+	ExpectNil(res, t)
+	ExpectInt(1, len(sut.content), t)
+	actual, ok := sut.content[ES_UNKNOWN]
+	ExpectTrue(ok, t)
+	ExpectString(expected, string(actual), t)
+}
+
 func TestThat_Transcoder_FromFile_SetterRetainsProperties_WithoutErrorResult(t *testing.T) {
 	// Setup
 	sut := NewTranscoder()
@@ -61,6 +96,23 @@ func TestThat_Transcoder_FromFile_SetterRetainsProperties_WithoutErrorResult(t *
 	ExpectNil(res, t)
 	ExpectInt(1, len(sut.content), t)
 	actual, ok := sut.content[ES_NONE]
+	ExpectTrue(ok, t)
+	ExpectString(expected, string(actual), t)
+}
+
+func TestThat_Transcoder_FromFile_SetterReplacesProperties_WithoutErrorResult(t *testing.T) {
+	// Setup
+	sut := NewTranscoder()
+	expected := "Vegetable Soup!"
+
+	// Testing
+	sut.FromFile("transcoder_test.vegetable_soup.txt", ES_NONE)
+	res := sut.FromFile("transcoder_test.vegetable_soup.txt", ES_UNKNOWN)
+
+	// Verify
+	ExpectNil(res, t)
+	ExpectInt(1, len(sut.content), t)
+	actual, ok := sut.content[ES_UNKNOWN]
 	ExpectTrue(ok, t)
 	ExpectString(expected, string(actual), t)
 }
