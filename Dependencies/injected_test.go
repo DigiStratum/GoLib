486b2e9597e09965bd05cd65b491fcefe605b88d
Sean Kelly <keenskelly@gmail.com>
2021-11-07 07:55:30 -0800

    WIP - Refactoring - Dependencies - Test coverage

--- Dependencies/injected_test.go
+++ Dependencies/injected_test.go
@@ -125,3 +125,97 @@ func TestThat_DependencyInjected_GetInvalidDependencyNames_ReturnsInvalidNames(t
 	// Verify
 	ExpectInt(expected, actual, t)
 }
+
+func TestThat_DependencyInjected_IsValid_ReturnsFalse(t *testing.T) {
+	var optionalNames, requiredNames []string
+	var deps *Dependencies
+	var sut *DependencyInjected
+
+	// Setup / Test : One required, nothing optional/injected
+	deps = NewDependencies()
+	sut = NewDependencyInjected(deps)
+	requiredNames = []string{"one"}
+	sut.SetRequired(&requiredNames)
+	res1 := sut.IsValid()
+	ExpectFalse(res1, t)
+
+	// Setup / Test : Two required, one required injected, nothing optional
+	deps = NewDependencies()
+	deps.Set("one", "sillystring!")
+	sut = NewDependencyInjected(deps)
+	requiredNames = []string{"one", "two"}
+	sut.SetRequired(&requiredNames)
+	res2 := sut.IsValid()
+	ExpectFalse(res2, t)
+
+	// Setup / Test : Nothing required, one optional, one invalid optional injected
+	deps = NewDependencies()
+	deps.Set("two", "sillystring!")
+	sut = NewDependencyInjected(deps)
+	optionalNames = []string{"one"}
+	sut.SetOptional(&optionalNames)
+	res3 := sut.IsValid()
+	ExpectFalse(res3, t)
+
+	// Setup / Test : Two required, one optional, one required injected, one invalid optional injected
+	deps = NewDependencies()
+	deps.Set("one", "sillystring!")
+	deps.Set("four", "sillystring!")
+	sut = NewDependencyInjected(deps)
+	requiredNames = []string{"one", "two"}
+	sut.SetRequired(&requiredNames)
+	optionalNames = []string{"three"}
+	sut.SetOptional(&optionalNames)
+	res4 := sut.IsValid()
+	ExpectFalse(res4, t)
+}
+
+func TestThat_DependencyInjected_IsValid_ReturnsTrue(t *testing.T) {
+	var optionalNames, requiredNames []string
+	var deps *Dependencies
+	var sut *DependencyInjected
+
+	// Setup / Test : Nothing required/optional/injected
+	deps = NewDependencies()
+	sut = NewDependencyInjected(deps)
+	res1 := sut.IsValid()
+	ExpectTrue(res1, t)
+
+	// Setup / Test : Nothing required/injected, two optional
+	deps = NewDependencies()
+	sut = NewDependencyInjected(deps)
+	optionalNames = []string{"one", "two"}
+	sut.SetOptional(&optionalNames)
+	res2 := sut.IsValid()
+	ExpectTrue(res2, t)
+
+	// Setup / Test : Nothing required, two optional, one optional injected
+	deps = NewDependencies()
+	deps.Set("one", "sillystring!")
+	sut = NewDependencyInjected(deps)
+	optionalNames = []string{"one", "two"}
+	sut.SetOptional(&optionalNames)
+	res3 := sut.IsValid()
+	ExpectTrue(res3, t)
+
+	// Setup / Test : Nothing optional, one required, one required injected
+	deps = NewDependencies()
+	deps.Set("one", "sillystring!")
+	sut = NewDependencyInjected(deps)
+	optionalNames = []string{"one"}
+	sut.SetOptional(&optionalNames)
+	res4 := sut.IsValid()
+	ExpectTrue(res4, t)
+
+	// Setup / Test : One optional, one required, one optional+required injected
+	deps = NewDependencies()
+	deps.Set("one", "sillystring!")
+	deps.Set("two", "sillystring!")
+	sut = NewDependencyInjected(deps)
+	optionalNames = []string{"one"}
+	sut.SetOptional(&optionalNames)
+	requiredNames = []string{"two"}
+	sut.SetRequired(&requiredNames)
+	res5 := sut.IsValid()
+	ExpectTrue(res5, t)
+}
