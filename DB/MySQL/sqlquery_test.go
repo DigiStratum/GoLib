4d36ab9ee76004f49384a75264993cdcfd1df411
Sean Kelly <keenskelly@gmail.com>
2022-05-12 06:55:21 -0700

    WIP - DB > MySQL test coverage

---
+++ DB/MySQL/sqlquery_test.go
@@ -0,0 +1,40 @@
+package mysql
+
+import(
+	"testing"
+
+	. "github.com/DigiStratum/GoLib/Testing"
+)
+
+func TestThat_NewSQLQuery_ReturnsSomething(t *testing.T) {
+	// Test
+	sut := NewSQLQuery("bogus query")
+
+	// Verify
+	ExpectNonNil(sut, t)
+}
+
+func TestThat_Resolve_ReturnsError_WhenNil(t *testing.T) {
+	// Setup
+	var sut SQLQuery
+
+	// Test
+	_, err := sut.Resolve()
+
+	// Verify
+	ExpectError(err, t)
+}
+
+func TestThat_Resolve_ReturnsQuery_NoError(t *testing.T) {
+	// Setup
+	expectedQuery := "bogus query"
+	sut := NewSQLQuery(expectedQuery)
+
+	// Test
+	actual, err := sut.Resolve()
+
+	// Verify
+	ExpectNoError(err, t)
+	ExpectString(expectedQuery, actual, t)
+}
+
