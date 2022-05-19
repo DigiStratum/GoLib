f614ca7e5dc0ff18641922433c09d621e0fae823
Sean Kelly <keenskelly@gmail.com>
2022-05-19 08:05:13 -0700

    WIP - Logger test coverage

---
+++ Logger/logger_test.go
@@ -0,0 +1,49 @@
+package logger
+
+import(
+	//"fmt"
+	"testing"
+
+	. "github.com/DigiStratum/GoLib/Testing"
+)
+
+var LastMessage *string
+type mockLogWriter struct { }
+
+func (r mockLogWriter) Log(message string) {
+	LastMessage = &message
+}
+
+func TestThat_GetLogger_ReturnsSomething(t *testing.T) {
+	// Test
+	var sut *Logger = GetLogger()
+
+	// Verify
+	ExpectNonNil(sut, t)
+}
+
+func TestThat_NewLogger_ReturnsSomething(t *testing.T) {
+	// Test
+	var sut *Logger = NewLogger("newstream")
+
+	// Verify
+	ExpectNonNil(sut, t)
+}
+
+// SetLogWriter(logWriter lw.LogWriterIfc)
+func TestThat_SetLogWriter_ReplacesStdOutWithMock(t *testing.T) {
+	// Setup
+	var sut *Logger = GetLogger()
+	mockWriter := mockLogWriter{}
+	expectedMessage := "test message"
+
+	// Test
+	sut.SetLogWriter(mockWriter)
+	sut.Error(expectedMessage)
+
+	// Verify
+	ExpectNonNil(LastMessage, t)
+	// FIXME: regex/partial match expectedMessage against: 2022-05-19T08:03:24-07:00 thread:1652972604971037495 ERROR test message
+	//	ExpectString(fmt.Sprintf("%s\n", expectedMessage), *LastMessage, t)
+}
+
