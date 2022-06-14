8840117e550639e7671a94976cdb03a3ca10c935
Sean Kelly <keenskelly@gmail.com>
2022-06-14 08:15:27 -0700

    WIP - Test coverage for EncodingSchemebase64

--- Data/transcoder/encodingscheme/encodingschemebase64_test.go
+++ Data/transcoder/encodingscheme/encodingschemebase64_test.go
@@ -37,19 +37,7 @@ func TestThat_EncodingSchemeBase64_SetEncodedValue_ReturnsError_WhenSourceIsNil(
         ExpectError(err, t)
 }
 
-func TestThat_EncodingSchemeBase64_SetEncodedValue_ReturnsNoError_WhenSourceIsString(t *testing.T) {
-	// Setup
-        sut := NewEncodingSchemeBase64()
-	testBase64 := "YWJjMTIzIT8kKiYoKSctPUB+"
-
-	// Test
-	err := sut.SetEncodedValue(&testBase64)
-
-        // Verify
-        ExpectNoError(err, t)
-}
-
-func TestThat_EncodingSchemeBase64_GetEncodedValue_ReturnsError_WhenRawIsNil(t *testing.T) {
+func TestThat_EncodingSchemeBase64_GetEncodedValue_ReturnsError_WhenSourceIsNil(t *testing.T) {
 	// Setup
         sut := NewEncodingSchemeBase64()
 
@@ -59,5 +47,79 @@ func TestThat_EncodingSchemeBase64_GetEncodedValue_ReturnsError_WhenRawIsNil(t *
         // Verify
         ExpectError(err, t)
 	ExpectNil(actual, t)
+}
+
+func TestThat_EncodingSchemeBase64_GetEncodedValue_ReturnsOriginalValue_WhenSourceIsGood(t *testing.T) {
+	// Setup
+        sut := NewEncodingSchemeBase64()
+	expected := "YWJjMTIzIT8kKiYoKSctPUB+"
+	err1 := sut.SetEncodedValue(&expected)
+
+	// Test
+	actual, err2 := sut.GetEncodedValue()
+
+        // Verify
+        ExpectNoError(err1, t)
+        ExpectNoError(err2, t)
+	ExpectNonNil(actual, t)
+	ExpectString(expected, *actual, t)
+}
+
+func TestThat_EncodingSchemeBase64_GetDecodedValue_ReturnsError_WhenSourceIsNil(t *testing.T) {
+	// Setup
+        sut := NewEncodingSchemeBase64()
+
+	// Test
+	actual, err := sut.GetDecodedValue()
+
+        // Verify
+        ExpectError(err, t)
+	ExpectNil(actual, t)
+}
+
+func TestThat_EncodingSchemeBase64_GetDecodedValue_ReturnsOriginalValue_WhenSourceIsGood(t *testing.T) {
+	// Setup
+        sut := NewEncodingSchemeBase64()
+	expected := "abc123!?$*&()'-=@~"
+	sut.SetDecodedValue(&expected)
+
+	// Test
+	actual, err := sut.GetDecodedValue()
+
+        // Verify
+        ExpectNoError(err, t)
+	ExpectNonNil(actual, t)
+}
+
+func TestThat_EncodingSchemeBase64_GetDecodedValue_ReturnsDecodedValue_WhenSourceIsEncoded(t *testing.T) {
+	// Setup
+        sut := NewEncodingSchemeBase64()
+	source:= "YWJjMTIzIT8kKiYoKSctPUB+"
+	sut.SetEncodedValue(&source)
+	expected := "abc123!?$*&()'-=@~"
+
+	// Test
+	actual, err := sut.GetDecodedValue()
+
+        // Verify
+        ExpectNoError(err, t)
+	ExpectNonNil(actual, t)
+	ExpectString(expected, *actual, t)
+}
+
+func TestThat_EncodingSchemeBase64_GetEncodedValue_ReturnsEncodedValue_WhenSourceIsDecoded(t *testing.T) {
+	// Setup
+        sut := NewEncodingSchemeBase64()
+	source := "abc123!?$*&()'-=@~"
+	sut.SetDecodedValue(&source)
+	expected := "YWJjMTIzIT8kKiYoKSctPUB+"
+
+	// Test
+	actual, err := sut.GetEncodedValue()
+
+        // Verify
+        ExpectNoError(err, t)
+	ExpectNonNil(actual, t)
+	ExpectString(expected, *actual, t)
 }
 
