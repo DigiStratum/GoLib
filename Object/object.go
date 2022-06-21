9c9cccdfc2eaaf93e26438309f0c978867f2f97e
Sean Kelly <keenskelly@gmail.com>
2022-06-20 19:57:42 -0700

    Add support for non-fielded Object content

--- Object/object.go
+++ Object/object.go
@@ -7,12 +7,27 @@ which can be managed via ObjectStore. By abstracting Objects as the data set ins
 file on disk, we can capture them in any number of places: files on disk, records in a database,
 representations in an API, even codified chunks of data within our own executable.
 
-Object optionally support fields; if the fields map is empty, then they are not being used.
+
+Object optionally support fields; if the fields map is empty, then they are not being used. In lieu
+of a populated field map, the content string is used. If it is nil then the entire Object's value
+is nil. Setting a content value purges the field map. Setting a field map nils the content.
+
+Objects are defined with fields by using AddField() to add each one. The defined fields may have a
+new value assigned at any time. The fields/values may also be defined by way of deserialization.
+But once the fields are defined for an object, the field definitions themselves are immutable: only
+the values ay be updated. In the case that the consumer wants to modify the field definitions of
+stored data for purposes of some sort of data transformation and/or data structure migration, it
+must first create a new Object and AddField() for all the new fields, and read the fields out of
+the old Object into the new one. We could potentially add a helper method with some sort of
+transform function to facilitate this should the need arise.
 
 TODO:
- * Refactor Object.fields to use a new of.ObjectFieldMap which extends lib.HashMap with support for of.ObjectFieldTypes
- * Cache the ToString() result so that successive calls return the same value until it is cleared by
+ * Refactor Object.fields to use a new of.ObjectFieldMap which extends lib.HashMap with support for
+   of.ObjectFieldTypes
+ * Cache Serialize() result so that successive calls return the same value until it is cleared by
    some change made by another method
+ * Add support for arbitrary field types, not just string (see SetFieldValue(); see also go 1.18
+   which supports generics)
 */
 
 import (
@@ -27,15 +42,17 @@ import (
 type ObjectIfc interface {
 	// Fields
 	AddField(fieldName string, value *string, ofType of.OFType) error
-	SetFieldValue(fieldName string, value *string) error
 	HasField(fieldName string) bool
 	GetFieldType(fieldName string) *of.ObjectFieldType
+	SetFieldValue(fieldName string, value *string) error
+	GetField(fieldName string) (*of.ObjectField, error)
 }
 
 // An Object that we're going to codify
 type Object struct {
 	transcoder		xc.TranscoderIfc
 	fields			map[string]*of.ObjectField		// Field name to value map
+	content			*string
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -54,9 +71,19 @@ func NewObject(transcoder xc.TranscoderIfc) *Object {
 // ObjectIfc Public Interface
 // -------------------------------------------------------------------------------------------------
 
-func (r *Object) AddField(fieldName string, value *string, ofType of.OFType) error {
+func (r *Object) SetContent(content *string) {
+	// Purge field map on setting content
+	r.fields = make(map[string]*of.ObjectField)
+	r.content = content
+}
 
-	// of.ObjectField Map needs a field with this name in place; create it if it's missing
+func (r Object) GetContent() *string {
+	return r.content
+}
+
+func (r *Object) AddField(fieldName string, value *string, ofType of.OFType) error {
+	// Purge string content on definition of a field map
+	if nil != r.content { r.content = nil }
 	if ! r.HasField(fieldName) {
 		objectField := of.NewObjectField()
 		objectField.Type = of.NewObjectFieldTypeFromOFType(ofType)
@@ -66,71 +93,74 @@ func (r *Object) AddField(fieldName string, value *string, ofType of.OFType) err
 	return r.SetFieldValue(fieldName, value)
 }
 
+func (r Object) HasField(fieldName string) bool {
+	_, ok := r.fields[fieldName]
+	return ok
+}
+
+func (r Object) GetFieldType(fieldName string) *of.ObjectFieldType {
+	if ! r.HasField(fieldName) { return nil }
+	return r.fields[fieldName].Type
+}
+
 // Set the named field to the specified value (including nil!)
 func (r *Object) SetFieldValue(fieldName string, value *string) error {
-
-	// of.ObjectField Map needs a field with this name in place
 	if objectField, ok := r.fields[fieldName]; ok {
-
-		// Validate the new value against the field's type
 		if ! objectField.Type.IsValid(value) {
 			return fmt.Errorf(
 				"Object Field [name: '%s', type: '%s'] does not match supplied value",
 				fieldName, objectField.Type.ToString(),
 			)
 		}
-
-		// Set the value, yey!
 		objectField.Value = value
 		r.fields[fieldName] = objectField
-	} else {
-		return fmt.Errorf("Object has no field named '%s'", fieldName)
+		return nil
 	}
-	return nil
+	return fmt.Errorf("Object has no field named '%s'", fieldName)
 }
 
-func (r Object) HasField(fieldName string) bool {
-	_, ok := r.fields[fieldName]
-	return ok
-}
-
-// Return the type of the named field
-func (r Object) GetFieldType(fieldName string) *of.ObjectFieldType {
-	if ! r.HasField(fieldName) { return nil }
-	return r.fields[fieldName].Type
+func (r *Object) GetField(fieldName string) (*of.ObjectField, error) {
+	if objectField, ok := r.fields[fieldName]; ok { return objectField, nil }
+	return nil, fmt.Errorf("Object has no field named '%s'", fieldName)
 }
 
 // -------------------------------------------------------------------------------------------------
 // JsonSerializableIfc Public Interface
 // -------------------------------------------------------------------------------------------------
 
+// JSON String result will be nil for error, start with doublequote
+// for string value, and open curly brace for fieldmapped value
 func (r *Object) ToJson() (*string, error) {
-	jsonBytes, err := json.Marshal(r.fields)
-	if nil != err { return nil, err }
-	jsonString := string(jsonBytes)
+	var jsonString string
+	if len(r.fields) > 0 {
+		jsonBytes, err := json.Marshal(r.fields)
+		if nil != err { return nil, err }
+		jsonString = string(jsonBytes)
+	} else if nil != r.content {
+		jsonBytes, err := json.Marshal(r.content)
+		if nil != err { return nil, err }
+		jsonString = string(jsonBytes)
+	} else {
+		jsonString = ""
+	}
 	return &jsonString, nil
 }
-
-/*
-// FIXME: If fields are in use, the json should be of the field map, otherwise, it should be of a non-fielded content string
-// We can probably just do a simple, hacky inspection of the JSON string to figure out if we are using fields by checking for '{'
-// as the first character which indicates that an object (JSON) follows rather than a plain string which would start with '"'
-
-func (r Object) ToJson() (*string, error) {
-	jsonBytes, err := json.Marshal(r.fields)
-	if nil != err { return nil, err }
-	jsonString := string(jsonBytes[:])
-	return &jsonString, nil
-}
-*/
 
 // -------------------------------------------------------------------------------------------------
 // JsonDeserializableIfc Public Interface
 // -------------------------------------------------------------------------------------------------
 
 func (r *Object) FromJson(jsonString *string) error {
+	// Purge field map and content
 	r.fields = make(map[string]*of.ObjectField)
-	return json.Unmarshal([]byte(*jsonString), &r.fields)
+	r.content = nil
+	// nil
+	if nil == jsonString { return nil }
+	// content string
+	if "\"" == string([]rune(*jsonString)[0]) { return json.Unmarshal([]byte(*jsonString), &r.content) }
+	// field mapped object
+	if "{" == string([]rune(*jsonString)[0]) { return json.Unmarshal([]byte(*jsonString), &r.fields) }
+	return fmt.Errorf("Unsupported JSON value")
 }
 
 // -------------------------------------------------------------------------------------------------
