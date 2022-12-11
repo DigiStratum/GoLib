d452114d98992d7b3e1545aa51fc0db2c0b9b3a7
Sean Kelly <keenskelly@gmail.com>
2022-12-10 19:29:04 -0800

    Add some notes re JsonSerializable vs. un|marshallJSON...

--- Data/json/jsonserializable.go
+++ Data/json/jsonserializable.go
@@ -1,5 +1,19 @@
 package json
 
+/*
+Our own take on JsonSerializable.
+
+Note that this is a weak substitute/attempt to "normalize" JSON support for our own library when we
+have these available from Go built-in packages:
+
+MarshalJSON() ([]byte, error)		// Equivalent of JsonSerializableIfc
+UnmarshalJSON(value []byte) error	// Equivalent of JsonDeserializableIfc
+
+The only difference, really, is []byte vs *string. If we want to seriously support reorienting around
+strings, then we probably need to support some sort of reflection/deep recursion mechanism that will
+traverse the structures received properly as with Un|MarshallJSON
+
+*/
 type JsonSerializableIfc interface {
 	ToJson() (*string, error)
 }
