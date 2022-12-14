7d47f260527d3fa6e86ac5b98676247a287212f9
Sean Kelly <keenskelly@gmail.com>
2022-12-13 22:18:40 -0800

    WIP - Add some followup notes

--- DB/Memcache/memcacheclient.go
+++ DB/Memcache/memcacheclient.go
@@ -7,6 +7,30 @@ Memcached data access
 We're going to use someone else's library behind the scenes here for now, but abstract it so that we
 can replace the library with something else without breaking consumers.
 
+
+from bradfitz/memcache/memcache.go:
+
+// Item is an item to be got or stored in a memcached server.
+type Item struct {
+        // Key is the Item's key (250 bytes maximum).
+        Key string
+
+        // Value is the Item's value.
+        Value []byte
+
+        // Flags are server-opaque flags whose semantics are entirely
+        // up to the app.
+        Flags uint32
+
+        // Expiration is the cache expiration time, in seconds: either a relative
+        // time from now (up to 1 month), or an absolute Unix epoch time.
+        // Zero means the Item has no expiration time.
+        Expiration int32
+
+        // Compare and swap ID.
+        casid uint64
+}
+
 */
 
 import (
