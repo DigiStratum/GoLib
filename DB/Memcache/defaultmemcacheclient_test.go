022d9ff45dcc4a39b5032c777f97868f007accd9
Sean Kelly <keenskelly@gmail.com>
2022-12-21 23:09:40 -0800

    WIP - Get the FakeMemcachedServer responding to version directive

--- DB/Memcache/defaultmemcacheclient_test.go
+++ DB/Memcache/defaultmemcacheclient_test.go
@@ -1,7 +1,10 @@
 package memcache
 
 import(
+	"fmt"
 	"net"
+	"sync"
+	"strings"
 	"testing"
 
 	. "github.com/DigiStratum/GoLib/Testing"
@@ -10,7 +13,7 @@ import(
 )
 
 const FAKE_MEMCACHED_PORT = 21212
-const FAKE_MEMCACHED_HOST = "localhost:" + FAKE_MEMCACHED_PORT
+const FAKE_MEMCACHED_HOST = "localhost"
 
 func TestThat_NewDefaultMemcacheClient_ReturnsError_WhenNoHostsSpecified(t *testing.T) {
 	// Setup
@@ -26,10 +29,7 @@ func TestThat_NewDefaultMemcacheClient_ReturnsError_WhenNoHostsSpecified(t *test
 
 func TestThat_NewDefaultMemcacheClient_ReturnsError_WhenTimeSourceNil(t *testing.T) {
 	// Setup
-	fakeServer, err := fakeMemcached()
-	ExpectNoError(err, t)
-        defer fakeServer.Close()
-	hosts := []string{ FAKE_MEMCACHED_HOST }
+	hosts := []string{ fmt.Sprintf("%s:%d", FAKE_MEMCACHED_HOST, FAKE_MEMCACHED_PORT) }
 
 	// Test
 	sut, err := NewDefaultMemcacheClient(nil, hosts...)
@@ -39,50 +39,98 @@ func TestThat_NewDefaultMemcacheClient_ReturnsError_WhenTimeSourceNil(t *testing
 	ExpectError(err, t)
 }
 
-type fakeMemcachedServer struct {
-	listener	net.Listener
-	terminate	bool
+func TestThat_NewDefaultMemcacheClient_ReturnsNonNil_NoError(t *testing.T) {
+	// Setup
+	fakeServer, err := newFakeMemcachedServer(true)
+	ExpectNoError(err, t)
+        defer fakeServer.Close()
+	ts := chrono.NewTimeSource()
+	hosts := []string{ fmt.Sprintf("%s:%d", FAKE_MEMCACHED_HOST, FAKE_MEMCACHED_PORT) }
+
+	// Test
+	sut, err := NewDefaultMemcacheClient(ts, hosts...)
+
+	// Verify
+	ExpectNonNil(sut, t)
+	ExpectNoError(err, t)
 }
 
-func newFakeMemcachedServer() (*fakeMemcachedServer, error) {
-	listener, err := net.Listen("tcp", FAKE_MEMCACHED_HOST)
+type fakeMemcachedServer struct {
+	host		string
+	listener	net.Listener
+	waitGroup	sync.WaitGroup	// ref: https://gobyexample.com/waitgroups
+	verbose		bool
+}
+
+func newFakeMemcachedServer(verbose bool) (*fakeMemcachedServer, error) {
+	host := fmt.Sprintf("%s:%d", FAKE_MEMCACHED_HOST, FAKE_MEMCACHED_PORT)
+	listener, err := net.Listen( "tcp", host )
 	if nil != err { return nil, err }
 	fms := fakeMemcachedServer{
 		listener:	listener,
-		terminate:	false,
+		verbose:	verbose,
+		host:		host,
 	}
 	// Spin off a Go Routine for the FMS Listener
 	go fms.Listen()
-	return &fms
+	return &fms, nil
 }
 
+// TODO: Any value in a generalized CloseableIfc to organize different Close()able types through?
 // ref: https://www.reddit.com/r/golang/comments/a4nim7/nonblocking_accept_for_tcp_connections/
-func (r *fakeMemcachedServer) Stop() {
+func (r *fakeMemcachedServer) Close() {
+	r.waitGroup.Wait()
 	r.listener.Close()
 }
 
 // ref: https://www.developer.com/languages/intro-socket-programming-go/
 func (r *fakeMemcachedServer) Listen() {
+	if r.verbose {
+		fmt.Printf("\tFakeMemcachedServer: Ready to Accept Connections on '%s'\n", r.host)
+	}
 	for {
 		connection, err := r.listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			break
+		if err == nil {
+			if r.verbose {
+				fmt.Println("\tFakeMemcachedServer: Got new client connection!\t")
+			}
+			go r.handleConnection(connection)
 		}
-		fmt.Println("client connected")
-		go r.handleConnection(connection)
 	}
 }
 
 // Now that we have a memcache client connected, let's interact
 func (r *fakeMemcachedServer) handleConnection(connection net.Conn) {
+	// Make sure the Server waits for us to finish before Close()ing the Listener
+	r.waitGroup.Add(1)
+	defer r.waitGroup.Done()
+
+	// Read a message
 	buffer := make([]byte, 1024)
 	mLen, err := connection.Read(buffer)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		if r.verbose {
+			fmt.Println("\tFakeMemcachedServer: Error reading:", err.Error())
+		}
+		return
 	}
-	fmt.Println("Received: ", string(buffer[:mLen]))
-	_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
+
+	// Got a message
+	// TODO: Break off just the first word as the directive
+	directive := strings.Trim(string(buffer[:mLen]), " \t\n\r")
+	switch directive {
+		case "version":
+			if r.verbose {
+				fmt.Printf("\tFakeMemcachedServer: Got 'version' directive!\n")
+			}
+			connection.Write([]byte("VERSION 0.0\n"))
+
+		default:
+			if r.verbose {
+				fmt.Printf("\tFakeMemcachedServer: Unhandled directive: '%s'\n", directive)
+			}
+	}
+
 	connection.Close()
 }
 
