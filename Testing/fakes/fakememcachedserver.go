80c9a59697cad2d70a8157fa7e52991b2084d079
Sean Kelly <keenskelly@gmail.com>
2022-12-25 18:33:36 -0800

    WIP - flush all for fakeMemcachedServer

--- Testing/fakes/fakememcachedserver.go
+++ Testing/fakes/fakememcachedserver.go
@@ -121,6 +121,159 @@ The return value is a list of statistics data, formatted as follows:
 
 The statistics are terminated with a single line, END.
 
+Stats:
+|-----------------------+---------+-------------------------------------------|
+| Name                  | Type    | Meaning                                   |
+|-----------------------+---------+-------------------------------------------|
+| pid                   | 32u     | Process id of this server process         |
+| uptime                | 32u     | Number of secs since the server started   |
+| time                  | 32u     | current UNIX time according to the server |
+| version               | string  | Version string of this server             |
+| pointer_size          | 32      | Default size of pointers on the host OS   |
+|                       |         | (generally 32 or 64)                      |
+| rusage_user           | 32u.32u | Accumulated user time for this process    |
+|                       |         | (seconds:microseconds)                    |
+| rusage_system         | 32u.32u | Accumulated system time for this process  |
+|                       |         | (seconds:microseconds)                    |
+| curr_items            | 64u     | Current number of items stored            |
+| total_items           | 64u     | Total number of items stored since        |
+|                       |         | the server started                        |
+| bytes                 | 64u     | Current number of bytes used              |
+|                       |         | to store items                            |
+| max_connections       | 32u     | Max number of simultaneous connections    |
+| curr_connections      | 32u     | Number of open connections                |
+| total_connections     | 32u     | Total number of connections opened since  |
+|                       |         | the server started running                |
+| rejected_connections  | 64u     | Conns rejected in maxconns_fast mode      |
+| connection_structures | 32u     | Number of connection structures allocated |
+|                       |         | by the server                             |
+| response_obj_oom      | 64u     | Connections closed by lack of memory      |
+| response_obj_count    | 64u     | Total response objects in use             |
+| response_obj_bytes    | 64u     | Total bytes used for resp. objects. is a  |
+|                       |         | subset of bytes from read_buf_bytes.      |
+| read_buf_count        | 64u     | Total read/resp buffers allocated         |
+| read_buf_bytes        | 64u     | Total read/resp buffer bytes allocated    |
+| read_buf_bytes_free   | 64u     | Total read/resp buffer bytes cached       |
+| read_buf_oom          | 64u     | Connections closed by lack of memory      |
+| reserved_fds          | 32u     | Number of misc fds used internally        |
+| proxy_conn_requests   | 64u     | Number of requests received by the proxy  |
+| proxy_conn_errors     | 64u     | Number of internal errors from proxy      |
+| proxy_conn_oom        | 64u     | Number of out of memory errors while      |
+|                       |         | serving proxy requests                    |
+| proxy_req_active      | 64u     | Number of in-flight proxy requests        |
+| cmd_get               | 64u     | Cumulative number of retrieval reqs       |
+| cmd_set               | 64u     | Cumulative number of storage reqs         |
+| cmd_flush             | 64u     | Cumulative number of flush reqs           |
+| cmd_touch             | 64u     | Cumulative number of touch reqs           |
+| get_hits              | 64u     | Number of keys that have been requested   |
+|                       |         | and found present                         |
+| get_misses            | 64u     | Number of items that have been requested  |
+|                       |         | and not found                             |
+| get_expired           | 64u     | Number of items that have been requested  |
+|                       |         | but had already expired.                  |
+| get_flushed           | 64u     | Number of items that have been requested  |
+|                       |         | but have been flushed via flush_all       |
+| delete_misses         | 64u     | Number of deletions reqs for missing keys |
+| delete_hits           | 64u     | Number of deletion reqs resulting in      |
+|                       |         | an item being removed.                    |
+| incr_misses           | 64u     | Number of incr reqs against missing keys. |
+| incr_hits             | 64u     | Number of successful incr reqs.           |
+| decr_misses           | 64u     | Number of decr reqs against missing keys. |
+| decr_hits             | 64u     | Number of successful decr reqs.           |
+| cas_misses            | 64u     | Number of CAS reqs against missing keys.  |
+| cas_hits              | 64u     | Number of successful CAS reqs.            |
+| cas_badval            | 64u     | Number of CAS reqs for which a key was    |
+|                       |         | found, but the CAS value did not match.   |
+| touch_hits            | 64u     | Number of keys that have been touched     |
+|                       |         | with a new expiration time                |
+| touch_misses          | 64u     | Number of items that have been touched    |
+|                       |         | and not found                             |
+| store_too_large       | 64u     | Number of rejected storage requests       |
+|                       |         | caused by attempting to write a value     |
+|                       |         | larger than the -I limit                  |
+| store_no_memory       | 64u     | Number of rejected storage requests       |
+|                       |         | caused by exhaustion of the -m memory     |
+|                       |         | limit (relevant when -M is used)          |
+| auth_cmds             | 64u     | Number of authentication commands         |
+|                       |         | handled, success or failure.              |
+| auth_errors           | 64u     | Number of failed authentications.         |
+| idle_kicks            | 64u     | Number of connections closed due to       |
+|                       |         | reaching their idle timeout.              |
+| evictions             | 64u     | Number of valid items removed from cache  |
+|                       |         | to free memory for new items              |
+| reclaimed             | 64u     | Number of times an entry was stored using |
+|                       |         | memory from an expired entry              |
+| bytes_read            | 64u     | Total number of bytes read by this server |
+|                       |         | from network                              |
+| bytes_written         | 64u     | Total number of bytes sent by this server |
+|                       |         | to network                                |
+| limit_maxbytes        | size_t  | Number of bytes this server is allowed to |
+|                       |         | use for storage.                          |
+| accepting_conns       | bool    | Whether or not server is accepting conns  |
+| listen_disabled_num   | 64u     | Number of times server has stopped        |
+|                       |         | accepting new connections (maxconns).     |
+| time_in_listen_disabled_us                                                  |
+|                       | 64u     | Number of microseconds in maxconns.       |
+| threads               | 32u     | Number of worker threads requested.       |
+|                       |         | (see doc/threads.txt)                     |
+| conn_yields           | 64u     | Number of times any connection yielded to |
+|                       |         | another due to hitting the -R limit.      |
+| hash_power_level      | 32u     | Current size multiplier for hash table    |
+| hash_bytes            | 64u     | Bytes currently used by hash tables       |
+| hash_is_expanding     | bool    | Indicates if the hash table is being      |
+|                       |         | grown to a new size                       |
+| expired_unfetched     | 64u     | Items pulled from LRU that were never     |
+|                       |         | touched by get/incr/append/etc before     |
+|                       |         | expiring                                  |
+| evicted_unfetched     | 64u     | Items evicted from LRU that were never    |
+|                       |         | touched by get/incr/append/etc.           |
+| evicted_active        | 64u     | Items evicted from LRU that had been hit  |
+|                       |         | recently but did not jump to top of LRU   |
+| slab_reassign_running | bool    | If a slab page is being moved             |
+| slabs_moved           | 64u     | Total slab pages moved                    |
+| crawler_reclaimed     | 64u     | Total items freed by LRU Crawler          |
+| crawler_items_checked | 64u     | Total items examined by LRU Crawler       |
+| lrutail_reflocked     | 64u     | Times LRU tail was found with active ref. |
+|                       |         | Items can be evicted to avoid OOM errors. |
+| moves_to_cold         | 64u     | Items moved from HOT/WARM to COLD LRU's   |
+| moves_to_warm         | 64u     | Items moved from COLD to WARM LRU         |
+| moves_within_lru      | 64u     | Items reshuffled within HOT or WARM LRU's |
+| direct_reclaims       | 64u     | Times worker threads had to directly      |
+|                       |         | reclaim or evict items.                   |
+| lru_crawler_starts    | 64u     | Times an LRU crawler was started          |
+| lru_maintainer_juggles                                                      |
+|                       | 64u     | Number of times the LRU bg thread woke up |
+| slab_global_page_pool | 32u     | Slab pages returned to global pool for    |
+|                       |         | reassignment to other slab classes.       |
+| slab_reassign_rescues | 64u     | Items rescued from eviction in page move  |
+| slab_reassign_evictions_nomem                                               |
+|                       | 64u     | Valid items evicted during a page move    |
+|                       |         | (due to no free memory in slab)           |
+| slab_reassign_chunk_rescues                                                 |
+|                       | 64u     | Individual sections of an item rescued    |
+|                       |         | during a page move.                       |
+| slab_reassign_inline_reclaim                                                |
+|                       | 64u     | Internal stat counter for when the page   |
+|                       |         | mover clears memory from the chunk        |
+|                       |         | freelist when it wasn't expecting to.     |
+| slab_reassign_busy_items                                                    |
+|                       | 64u     | Items busy during page move, requiring a  |
+|                       |         | retry before page can be moved.           |
+| slab_reassign_busy_deletes                                                  |
+|                       | 64u     | Items busy during page move, requiring    |
+|                       |         | deletion before page can be moved.        |
+| log_worker_dropped    | 64u     | Logs a worker never wrote due to full buf |
+| log_worker_written    | 64u     | Logs written by a worker, to be picked up |
+| log_watcher_skipped   | 64u     | Logs not sent to slow watchers.           |
+| log_watcher_sent      | 64u     | Logs written to watchers.                 |
+| log_watchers          | 64u     | Number of currently active watchers.      |
+| unexpected_napi_ids   | 64u     | Number of times an unexpected napi id is  |
+|                       |         | is received. See doc/napi_ids.txt         |
+| round_robin_fallback  | 64u     | Number of times napi id of 0 is received  |
+|                       |         | resulting in fallback to round robin      |
+|                       |         | thread selection. See doc/napi_ids.txt    |
+|-----------------------+---------+-------------------------------------------|
+
 -----
 
 Command		Command Formats
@@ -334,7 +487,7 @@ func (r *fakeMemcachedServer) handleConnection(connection net.Conn) {
 		r.vprintf("Empty command, nothing to do!")
 		return
 	}
-	var response string
+	response := ""		// Empty response is default ("noreply" reinforces this)
 	switch commandWords[0] {
 		// TODO: Add other commands
 		case "version":
@@ -401,6 +554,13 @@ func (r *fakeMemcachedServer) handleConnection(connection net.Conn) {
 		case "flush_all":
 			// Invalidate all existing cache items. Optionally takes a parameter, which means to invalidate all items after N seconds have passed.
 			// This command does not pause the server, as it returns immediately. It does not free up or flush memory at all, it just causes all items to expire.
+			r.cache = make(map[string]fakeCacheItem)
+			if len(commandWords) >= 2 {
+				next := commandWords[1]
+				if "noreply" != next {
+					response = r.getOkResponse()
+				}
+			}
 		default:
 			r.vprintf("Unhandled directive: '%s'", commandWords[0])
 			return
@@ -446,6 +606,9 @@ func (r *fakeMemcachedServer) getValueResponse(key string, ci *fakeCacheItem) st
 	)
 }
 
+func (r *fakeMemcachedServer) getOkResponse() string {
+	return fmt.Sprintf("OK\r\n")
+}
 
 // Put out a verbose message, ala Printf formatting
 func (r *fakeMemcachedServer) vprintf(formatter string, args ...interface{}) {
