1dd0153351a64471b14b41ddb4c18412a9f0aa4a
Sean Kelly <keenskelly@gmail.com>
2023-02-16 23:06:55 -0800

    WIP - Refine Dependencies example code. More TODO...!

--- Dependencies/example/dependencies_example.go
+++ Dependencies/example/dependencies_example.go
@@ -13,6 +13,7 @@ func (r *usefulService) UsefulActivity() {
 	fmt.Println("Useful activity output!")
 }
 
+/*
 func (r *usefulService) InjectInto(client dep.DependencyInjectableIfc, variant ...string) error {
 	v := dep.DEP_VARIANT_DEFAULT
 	for _, v = range variant {}
@@ -20,21 +21,63 @@ func (r *usefulService) InjectInto(client dep.DependencyInjectableIfc, variant .
 		dep.NewDependencyInstance("usefulService", r).SetVariant(v),
 	)
 }
+*/
 
 type dependentLayer struct {
 	*dep.DependencyInjected
+	us				*usefulService
+}
+
+// DependencyInjectableIfc
+// Override InjectDependencies()
+func (r *dependentLayer) InjectDependencies(depinst ...DependencyInstanceIfc) error {
+	err := r.DependencyInjected.InjectDependencies(depinst...)
+	if nil != err { return err }
+
+	// TODO: Move this to a DependencyInjected function to analyze and return an error or nil, maybe the InjectDependencies method?
+	if ! r.DependencyInjected.HasAllRequiredDependencies() {
+		missingDeps := r.DependencyInjected.GetMissingDependencies()
+		var sb strings.Builder
+		var delim := ""
+		for name, variants := range missingDeps.GetVariants() {
+			for _, variant := range variants {
+				sb.WriteString(fmt.Sprintf("%s%s:%s", delim, name, variant))
+				delim := ", "
+			}
+		}
+		missingList := sb.String()
+		return fmt.Errorf("Missing one or more required dependencies: %s", missingList)
+	}
+
+	// TODO: Iterate over injected dependencies; use a switch-case to map them to the correct interface assertion and member value
+	for name, variants := range r.DependencyInjected.GetVariants() {
+
+	}
+
+	/*
+	usdep := r.DependencyInjected.GetInstance("usefulService")
+
+	if nil != usdep {
+		if us, ok := d.(*usefulService); ok { r.us = us }
+	}
+	*/
 }
 
 func (r *dependentLayer) DoWork() {
+/*
 	var uids *[]string = r.DependencyInjected.GetUniqueIds()
 	//if (nil == uids) || (0 == len(*uids)) { fmt.Println("Seems to be empty...") }
 	var uid  string = (*uids)[0]
 	//fmt.Printf("Found UID: '%s'\n", uid)
 	d := r.DependencyInjected.GetInstance(uid)
-	if us, ok := d.(*usefulService); ok {
-		us.UsefulActivity()
-	} else {
-		fmt.Println("Doesn't seem to be a usefulService...")
+*/
+	usdep := r.DependencyInjected.GetInstance("usefulService")
+	if nil != usdep {
+		if us, ok := d.(*usefulService); ok {
+			us.UsefulActivity()
+		} else {
+			fmt.Println("Doesn't seem to be a usefulService...")
+		}
 	}
 	//if nil == d { fmt.Printf("Seems to be nil... [%s]\n", uid) }
 }
@@ -48,8 +91,15 @@ func main() {
 		),
 	}
 
-	us := &usefulService{}
-	us.InjectInto(dl)
+	//us := &usefulService{}
+	//us.InjectInto(dl)
+
+	dl.InjectDependecies(
+		dep.NewDependencyInstance(
+			dep.NewDependency("usefulService"),
+			&usefulService{},
+		),
+	)
 
 	dl.DoWork();
 }
