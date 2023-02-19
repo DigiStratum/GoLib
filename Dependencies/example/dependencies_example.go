59962330e702ae9a02a4c246368e04c6565294e7
Sean Kelly <keenskelly@gmail.com>
2023-02-18 21:06:36 -0800

    WIP - Get example code working
    Still a few TODO's remain; these are important to think through because the entire point of this exercise is to standardize our approach to dependency injection and reduce the amount of boilerplate code that ends up in our applicatoin layers. The more that can be baked into the library, the better. The biggest challenge WRT elimination of boilerplate seems to be the handling of of interface assertions which don't support dynamic typing (i.e. you can't pass the name of an interface as a string and get what you want out of it)

--- Dependencies/example/dependencies_example.go
+++ Dependencies/example/dependencies_example.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	dep "github.com/DigiStratum/GoLib/Dependencies"
 )
+
+type usefulServiceIfc interface {
+	UsefulActivity()
+}
 
 type usefulService struct {
 }
@@ -13,24 +18,14 @@ func (r *usefulService) UsefulActivity() {
 	fmt.Println("Useful activity output!")
 }
 
-/*
-func (r *usefulService) InjectInto(client dep.DependencyInjectableIfc, variant ...string) error {
-	v := dep.DEP_VARIANT_DEFAULT
-	for _, v = range variant {}
-	return client.InjectDependencies(
-		dep.NewDependencyInstance("usefulService", r).SetVariant(v),
-	)
-}
-*/
-
 type dependentLayer struct {
 	*dep.DependencyInjected
-	us				*usefulService
+	us				usefulServiceIfc
 }
 
 // DependencyInjectableIfc
 // Override InjectDependencies()
-func (r *dependentLayer) InjectDependencies(depinst ...DependencyInstanceIfc) error {
+func (r *dependentLayer) InjectDependencies(depinst ...dep.DependencyInstanceIfc) error {
 	err := r.DependencyInjected.InjectDependencies(depinst...)
 	if nil != err { return err }
 
@@ -38,48 +33,41 @@ func (r *dependentLayer) InjectDependencies(depinst ...DependencyInstanceIfc) er
 	if ! r.DependencyInjected.HasAllRequiredDependencies() {
 		missingDeps := r.DependencyInjected.GetMissingDependencies()
 		var sb strings.Builder
-		var delim := ""
+		delim := ""
 		for name, variants := range missingDeps.GetVariants() {
 			for _, variant := range variants {
 				sb.WriteString(fmt.Sprintf("%s%s:%s", delim, name, variant))
-				delim := ", "
+				delim = ", "
 			}
 		}
 		missingList := sb.String()
 		return fmt.Errorf("Missing one or more required dependencies: %s", missingList)
 	}
 
-	// TODO: Iterate over injected dependencies; use a switch-case to map them to the correct interface assertion and member value
+	// Iterate over injected dependencies; use a switch-case to map them to the correct interface assertion and member value
+	// TODO: Add some variant iterator with callback function per each?
 	for name, variants := range r.DependencyInjected.GetVariants() {
-
+		switch name {
+			case "usefulService":
+				// Any variant wins
+				for _, variant := range variants {
+					usdep := r.DependencyInjected.GetInstanceVariant(name, variant)
+					if nil != usdep {
+						if us, ok := usdep.(usefulServiceIfc); ok { r.us = us }
+					}
+				}
+		}
 	}
 
-	/*
-	usdep := r.DependencyInjected.GetInstance("usefulService")
-
-	if nil != usdep {
-		if us, ok := d.(*usefulService); ok { r.us = us }
-	}
-	*/
+	return nil
 }
 
 func (r *dependentLayer) DoWork() {
-/*
-	var uids *[]string = r.DependencyInjected.GetUniqueIds()
-	//if (nil == uids) || (0 == len(*uids)) { fmt.Println("Seems to be empty...") }
-	var uid  string = (*uids)[0]
-	//fmt.Printf("Found UID: '%s'\n", uid)
-	d := r.DependencyInjected.GetInstance(uid)
-*/
-	usdep := r.DependencyInjected.GetInstance("usefulService")
-	if nil != usdep {
-		if us, ok := d.(*usefulService); ok {
-			us.UsefulActivity()
-		} else {
-			fmt.Println("Doesn't seem to be a usefulService...")
-		}
+	if nil != r.us {
+		r.us.UsefulActivity()
+	} else {
+		fmt.Println("Doesn't seem to be a usefulService...")
 	}
-	//if nil == d { fmt.Printf("Seems to be nil... [%s]\n", uid) }
 }
 
 func main() {
@@ -91,14 +79,8 @@ func main() {
 		),
 	}
 
-	//us := &usefulService{}
-	//us.InjectInto(dl)
-
-	dl.InjectDependecies(
-		dep.NewDependencyInstance(
-			dep.NewDependency("usefulService"),
-			&usefulService{},
-		),
+	dl.InjectDependencies(
+		dep.NewDependencyInstance("usefulService", &usefulService{}),
 	)
 
 	dl.DoWork();
