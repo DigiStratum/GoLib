2c899ff60c93b11a7fddb2d5360530e265b81999
Sean Kelly <keenskelly@gmail.com>
2023-02-19 07:44:00 -0800

    WIP Move required deps validation to library from example
    Reduces the footprint of the example code, and therefore what is required of real implementations in terms of boilerplay, reusable code.

--- Dependencies/example/dependencies_example.go
+++ Dependencies/example/dependencies_example.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	dep "github.com/DigiStratum/GoLib/Dependencies"
 )
@@ -26,23 +25,8 @@ type dependentLayer struct {
 // DependencyInjectableIfc
 // Override InjectDependencies()
 func (r *dependentLayer) InjectDependencies(depinst ...dep.DependencyInstanceIfc) error {
-	err := r.DependencyInjected.InjectDependencies(depinst...)
-	if nil != err { return err }
-
-	// TODO: Move this to a DependencyInjected function to analyze and return an error or nil, maybe the InjectDependencies method?
-	if ! r.DependencyInjected.HasAllRequiredDependencies() {
-		missingDeps := r.DependencyInjected.GetMissingDependencies()
-		var sb strings.Builder
-		delim := ""
-		for name, variants := range missingDeps.GetVariants() {
-			for _, variant := range variants {
-				sb.WriteString(fmt.Sprintf("%s%s:%s", delim, name, variant))
-				delim = ", "
-			}
-		}
-		missingList := sb.String()
-		return fmt.Errorf("Missing one or more required dependencies: %s", missingList)
-	}
+	if err := r.DependencyInjected.InjectDependencies(depinst...); nil != err { return err }
+	if err := r.DependencyInjected.ValidateRequiredDependencies(); nil != err { return err }
 
 	// Iterate over injected dependencies; use a switch-case to map them to the correct interface assertion and member value
 	// TODO: Add some variant iterator with callback function per each?
@@ -71,6 +55,7 @@ func (r *dependentLayer) DoWork() {
 }
 
 func main() {
+	// Declare Dependencies
 	dl := &dependentLayer{
 		DependencyInjected: dep.NewDependencyInjected(
 			dep.NewDependencies(
@@ -79,10 +64,12 @@ func main() {
 		),
 	}
 
+	// Inject Dependencies
 	dl.InjectDependencies(
 		dep.NewDependencyInstance("usefulService", &usefulService{}),
 	)
 
+	// Do something useful!
 	dl.DoWork();
 }
 
