173f9f7f910832521019633ac20a7a9f583c4c96
Sean Kelly <keenskelly@gmail.com>
2023-03-06 22:15:08 -0800

    WIP - Get Strkeys() to a working state

--- Data/maps/maps.go
+++ Data/maps/maps.go
@@ -5,12 +5,20 @@ Functions and constructs for handling map objects
 
 */
 
+import (
+	"reflect"
+)
+
 // Get the set of keys for maps keyed with string
 // We are expecting m to be a map[string]interface{} of some sort...
 func Strkeys(m interface{}) []string {
 	mkeys := []string{}
-	if mi, ok := m.(map[string]interface{}); ok {
-		for key, _ := range mi { mkeys = append(mkeys, key) }
+	v := reflect.ValueOf(m)
+	if v.Kind() == reflect.Map {
+		keyValues := v.MapKeys()
+		for _, kV := range keyValues {
+			mkeys = append(mkeys, kV.String())
+		}
 	}
 	return mkeys
 }
