7655f3a67f2151882b2babaed0028d57c0ca3224
Sean Kelly <keenskelly@gmail.com>
2023-03-07 10:57:50 -0800

    Add bool return to Testing.Expect* functions
    This allows the caller to detect the failure and abort the remaining test conditions so that we don't see multiple, successive failures and surprising results. (Like a check for NonNil that fails should not continue and end up throwing memory access errors due to being nil, etc)

--- Testing/expect.go
+++ Testing/expect.go
@@ -41,83 +41,101 @@ func getCaller() string {
 	return ""
 }
 
-func ExpectEmptyString(actual string, t *testing.T) {
-	if 0 == len(actual) { return }
+func ExpectEmptyString(actual string, t *testing.T) bool {
+	if 0 == len(actual) { return true }
 	t.Errorf("\n\n%s:\nExpect len('%s') == 0, Actual: %d", getCaller(), actual, len(actual))
+	return false
 }
 
-func ExpectNonEmptyString(actual string, t *testing.T) {
-	if 0 < len(actual) { return }
+func ExpectNonEmptyString(actual string, t *testing.T) bool {
+	if 0 < len(actual) { return true }
 	t.Errorf("\n\n%s:\nExpect len('%s') > 0, Actual: %d", getCaller(), actual, len(actual))
+	return false
 }
 
-func ExpectString(expect, actual string, t *testing.T) {
-	if expect == actual { return }
+func ExpectString(expect, actual string, t *testing.T) bool {
+	if expect == actual { return true }
 	t.Errorf("\n\n%s:\nExpect: '%s', Actual: '%s'", getCaller(), expect, actual)
+	return false
 }
 
-func ExpectInt(expect, actual int, t *testing.T) {
-	if expect == actual { return }
+func ExpectInt(expect, actual int, t *testing.T) bool {
+	if expect == actual { return true }
 	t.Errorf("\n\n%s\nExpect: '%d', Actual: '%d'", getCaller(), expect, actual)
+	return false
 }
 
-func ExpectInt64(expect, actual int64, t *testing.T) {
-	if expect == actual { return }
+func ExpectInt64(expect, actual int64, t *testing.T) bool {
+	if expect == actual { return true }
 	t.Errorf("\n\n%s\nExpect: '%d', Actual: '%d'", getCaller(), expect, actual)
+	return false
 }
 
-func ExpectFloat64(expect, actual float64, t *testing.T) {
-	if math.Abs(expect - actual) < 0.0001 { return }
+func ExpectFloat64(expect, actual float64, t *testing.T) bool {
+	if math.Abs(expect - actual) < 0.0001 { return true }
 	t.Errorf("\n\n%s\nExpect: '%f', Actual: '%f'", getCaller(), expect, actual)
+	return false
 }
 
-func ExpectBool(expect, actual bool, t *testing.T) {
-	if expect == actual { return }
+func ExpectBool(expect, actual bool, t *testing.T) bool {
+	if expect == actual { return true }
 	t.Errorf("\n\n%s:\nExpect: '%t', Actual: '%t'", getCaller(), expect, actual)
+	return false
 }
 
-func ExpectTrue(actual bool, t *testing.T) {
-	if true == actual { return }
+func ExpectTrue(actual bool, t *testing.T) bool {
+	if true == actual { return true }
 	t.Errorf("\n\n%s:\nExpect: 'true', Actual: '%t'", getCaller(), actual)
+	return false
 }
 
-func ExpectFalse(actual bool, t *testing.T) {
-	if false == actual { return }
+func ExpectFalse(actual bool, t *testing.T) bool {
+	if false == actual { return true }
 	t.Errorf("\n\n%s:\nExpect: 'false', Actual: '%t'", getCaller(), actual)
+	return false
 }
 
 // ref: https://stackoverflow.com/questions/31595791/how-to-test-panics
-func ExpectPanic(t *testing.T) {
+func ExpectPanic(t *testing.T) bool {
 	if r := recover(); r == nil {
 		t.Errorf("\n\n%s:\nExpect: Panic, Actual: Nope!", getCaller())
+		return false
 	}
+	return true
 }
 
-func ExpectNil(value interface{}, t *testing.T) {
+func ExpectNil(value interface{}, t *testing.T) bool {
 	if value == nil || (reflect.ValueOf(value).Kind() == reflect.Ptr && reflect.ValueOf(value).IsNil()) {
-		return
+		return true
 	}
 	t.Errorf("\n\n%s:\nExpect: nil, Actual: non-nil", getCaller())
+	return false
 }
 
-func ExpectNonNil(value interface{}, t *testing.T) {
+func ExpectNonNil(value interface{}, t *testing.T) bool {
 	if value == nil || (reflect.ValueOf(value).Kind() == reflect.Ptr && reflect.ValueOf(value).IsNil()) {
 		t.Errorf("\n\n%s:\nExpect: non-nil, Actual: nil", getCaller())
+		return false
 	}
+	return true
 }
 
-func ExpectError(err error, t *testing.T) {
-	if nil != err {	return }
+func ExpectError(err error, t *testing.T) bool {
+	if nil != err {	return true }
 	t.Errorf("\n\n%s:\nExpect: error, Actual: nil error", getCaller())
+	return false
 }
 
-func ExpectNoError(err error, t *testing.T) {
-	if nil == err {	return }
+func ExpectNoError(err error, t *testing.T) bool {
+	if nil == err {	return true }
 	t.Errorf("\n\n%s:\nExpect: nil error, Actual: error('%s')", getCaller(), err.Error())
+	return false
 }
 
-func ExpectMatch(expect, actual string, t *testing.T) {
+func ExpectMatch(expect, actual string, t *testing.T) bool {
 	matched, err := regexp.MatchString(expect, actual)
-	if nil == err && matched { return }
+	if nil == err && matched { return true }
 	t.Errorf("\n\n%s:\nExpect: pattern [%s] match actual [%s], Actual: no match", getCaller(), expect, actual)
+	return false
 }
+
