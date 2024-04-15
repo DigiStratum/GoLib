2b15a5b17ad667cb54e1d56aad99106eb629b2cf
Sean Kelly <keenskelly@gmail.com>
2024-04-14 17:27:47 -0700

    Complete test coverage for FileIO file struct

--- FileIO/file_test.go
+++ FileIO/file_test.go
@@ -16,7 +16,7 @@ func TestThat_NewFile_ReturnsSomething(t *testing.T) {
 	ExpectNonNil(sut, t)
 }
 
-func TestThat_NewFile_GetPath_Returns_OriginalPath(t *testing.T) {
+func TestThat_File_GetPath_Returns_OriginalPath(t *testing.T) {
 	// Setup
 	expected := "missingfile.txt"
 	sut := NewFile(expected)
@@ -29,7 +29,7 @@ func TestThat_NewFile_GetPath_Returns_OriginalPath(t *testing.T) {
 }
 
 // TODO: Figure out some incantation that throws an error instead of resolved path
-func TestThat_NewFile_GetAbsPath_Returns_GoodPath(t *testing.T) {
+func TestThat_File_GetAbsPath_Returns_GoodPath(t *testing.T) {
 	// Setup
 	_, filename, _, _ := runtime.Caller(0)
 	expected := filepath.Dir(filename) + "/missingfile.txt"
@@ -43,7 +43,7 @@ func TestThat_NewFile_GetAbsPath_Returns_GoodPath(t *testing.T) {
 	ExpectString(expected, *actual, t)
 }
 
-func TestThat_NewFile_Exists_Returns_False_ForMissingFile(t *testing.T) {
+func TestThat_File_Exists_Returns_False_ForMissingFile(t *testing.T) {
 	// Setup
 	sut := NewFile("missingfile.txt")
 
@@ -52,5 +52,167 @@ func TestThat_NewFile_Exists_Returns_False_ForMissingFile(t *testing.T) {
 
 	// Verify
 	ExpectFalse(actual, t)
+}
+
+func TestThat_File_Exists_Returns_True_ForGoodFile(t *testing.T) {
+	// Setup
+	sut := NewFile("testfile.txt")
+
+	// Test
+	actual := sut.Exists()
+
+	// Verify
+	ExpectTrue(actual, t)
+}
+
+func TestThat_File_GetName_Returns_Name_ForGoodFile(t *testing.T) {
+	// Setup
+	expected := "testfile.txt"
+	sut := NewFile(expected)
+
+	// Test
+	actual, err := sut.GetName()
+
+	// Verify
+	ExpectNoError(err, t)
+	ExpectNonNil(actual, t)
+	ExpectString(expected, *actual, t)
+}
+
+func TestThat_File_GetName_Returns_Error_ForMissingFile(t *testing.T) {
+	// Setup
+	expected := "missingfile.txt"
+	sut := NewFile(expected)
+
+	// Test
+	actual, err := sut.GetName()
+
+	// Verify
+	ExpectError(err, t)
+	ExpectNil(actual, t)
+}
+
+func TestThat_File_GetSize_Returns_Error_ForMissingFile(t *testing.T) {
+	// Setup
+	expected := "missingfile.txt"
+	sut := NewFile(expected)
+
+	// Test
+	actual, err := sut.GetSize()
+
+	// Verify
+	ExpectError(err, t)
+	ExpectNil(actual, t)
+}
+
+func TestThat_File_GetSize_Returns_Number_ForGoodFile(t *testing.T) {
+	// Setup
+	sut := NewFile("testfile.txt")
+
+	// Test
+	actual, err := sut.GetSize()
+
+	// Verify
+	ExpectNoError(err, t)
+	ExpectNonNil(actual, t)
+	ExpectInt64(21, *actual, t)
+}
+
+func TestThat_File_GetMode_Returns_Error_ForMissingFile(t *testing.T) {
+	// Setup
+	expected := "missingfile.txt"
+	sut := NewFile(expected)
+
+	// Test
+	actual, err := sut.GetMode()
+
+	// Verify
+	ExpectError(err, t)
+	ExpectNil(actual, t)
+}
+
+func TestThat_File_GetMode_Returns_FileMode_ForGoodFile(t *testing.T) {
+	// Setup
+	sut := NewFile("testfile.txt")
+
+	// Test
+	actual, err := sut.GetMode()
+
+	// Verify
+	ExpectNoError(err, t)
+	ExpectNonNil(actual, t)
+}
+
+func TestThat_File_GetModTime_Returns_Error_ForMissingFile(t *testing.T) {
+	// Setup
+	expected := "missingfile.txt"
+	sut := NewFile(expected)
+
+	// Test
+	actual, err := sut.GetModTime()
+
+	// Verify
+	ExpectError(err, t)
+	ExpectNil(actual, t)
+}
+
+func TestThat_File_GetModTime_Returns_Time_ForGoodFile(t *testing.T) {
+	// Setup
+	sut := NewFile("testfile.txt")
+
+	// Test
+	actual, err := sut.GetModTime()
+
+	// Verify
+	ExpectNoError(err, t)
+	ExpectNonNil(actual, t)
+}
+
+func TestThat_File_IsDir_Returns_Error_ForMissingFile(t *testing.T) {
+	// Setup
+	expected := "missingdir"
+	sut := NewFile(expected)
+
+	// Test
+	_, err := sut.IsDir()
+
+	// Verify
+	ExpectError(err, t)
+}
+
+func TestThat_File_IsDir_Returns_True_ForGoodFileDir(t *testing.T) {
+	// Setup
+	sut := NewFile(".")
+
+	// Test
+	actual, err := sut.IsDir()
+
+	// Verify
+	ExpectNoError(err, t)
+	ExpectTrue(actual, t)
+}
+
+func TestThat_File_GetSys_Returns_Error_ForMissingFile(t *testing.T) {
+	// Setup
+	expected := "missingdir"
+	sut := NewFile(expected)
+
+	// Test
+	_, err := sut.GetSys()
+
+	// Verify
+	ExpectError(err, t)
+}
+
+func TestThat_File_GetSys_Returns_Something_ForGoodFileDir(t *testing.T) {
+	// Setup
+	sut := NewFile("testfile.txt")
+
+	// Test
+	actual, err := sut.GetSys()
+
+	// Verify
+	ExpectNoError(err, t)
+	ExpectNonNil(actual, t)
 }
 
