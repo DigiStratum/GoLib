495a7e7d29a7521339cb8296c7a04411a88dd497
Sean Kelly <keenskelly@gmail.com>
2024-05-11 08:46:29 -0700

    Add JsonTree tokenizer notes

--- Data/json/jsontree.go
+++ Data/json/jsontree.go
@@ -6,8 +6,7 @@ Represent a JSON structure as an object tree with JavaScript-like selectors and 
 
 Structuress under consideration:
 
-NIL:
-  ''
+NULL:
   'null'
 
 STRING:
@@ -27,6 +26,37 @@ OBJECT:
 ARRAY:
   '[]'
   '[null, "a", 1, 3.14, false, {}, {"a": "b"}, [], ["apples"]]'
+
+Tokenizer notes:
+ * Parse JSON string character by character
+   * Determine how to do this safely for multi-byte glyphs
+   * Default to UTF-8, allow override to maximize utility value
+ * Consume whitespace between tokens
+ * Initial state: expect ValueToken
+ * For each ValueToken:
+   1) Assert ValueToken.Type based on first character only:
+     NULL 	-> /[n]/i
+     STRING 	-> /["]/
+     NUMBER 	-> /[0-9]/
+     BOOL 	-> /[tf]]i
+     OBJECT 	-> /[\{]/
+     ARRAY 	-> /[\[]/
+
+   2) Append subsequent chars to ValueToken.RawValue until expected terminator char based on ValueToken.Type:
+     NULL	-> whitespace|EOF
+     STRING	-> /["]/ (handle escapes; beware of escaped escapes like '\\"'; the escape is escaped, not the quote!)
+     NUMBER	-> whitespace|EOF (allow anything in, fail in validation for non-numeric garbage)
+     BOOL	-> whitespace|EOF (allow anything in, fail in validation for non-boolean garbage)
+     OBJECT	-> enter recursion to expect NameToken : ValueToken until /[\}]/ clean close
+     ARRAY	-> enter recursion to expect ValueToken, repeat for each [,] until /[\]]/ clean close
+
+   3) Validate ValueToken.RawValue based on ValueToken.Type (require ValueToken.CleanClose == true)
+     NULL	-> /^null$/i
+     STRING	-> /^".*"$/
+     NUMBER 	-> /^[0-9]+(\.[0-9]+)*$/
+     BOOL	-> /^(true|false)$/i
+     OBJECT	-> [NameToken:ValueToken] collection, non-regex
+     ARRAY	-> [ValueToken] collection, non-regex
 
 TODO:
  * If we make our own tokenizer/parser, then we can unmarshall into a custom tree structure
