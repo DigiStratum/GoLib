7bf9d3ea8d68640a0517672ecec8ca3891034830
Sean Kelly <keenskelly@gmail.com>
2025-06-11 08:04:48 -0700

    Expand a comment for clarification on request body data

--- Net/http/httprequestbodydata.go
+++ Net/http/httprequestbodydata.go
@@ -3,7 +3,12 @@ package http
 /*
 Data set for HTTP request body data which supports multiple values for a given named property
 
-This is typically form-encoded data, parsing required
+This is typically form-encoded data, parsing required; note that this structure is important both
+for our code to create requets and send to a server... and also for us to receive requests from
+some client and act as the server. In the one case we must build the request and format it to send,
+and in the other we must parse the request body data to extract the name-value pairs so that we
+can respond in kind. In both cases, it is still a request body whether a request we are making or
+one that we are receiving.
 
 TODO:
  * Implement based on the Headers/Builder; this differs from Headers in that there are no name-
