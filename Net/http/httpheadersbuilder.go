720461045bc811f1767c3815aef1f5c1d2df61bd
Sean Kelly <keenskelly@gmail.com>
2025-06-11 21:56:39 -0700

    Fix the broken HttpHeaderBuilderIfc.Set() method

--- Net/http/httpheadersbuilder.go
+++ Net/http/httpheadersbuilder.go
@@ -4,9 +4,6 @@ package http
 
 Build a set of HTTP Headers for Request or Response
 
-FIXME:
- * Set function got entirely broken by refactoring
-
 TODO:
  * Consider direct support with validation for specific, known header names
    ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers
@@ -45,20 +42,12 @@ func NewHttpHeadersBuilder() *httpHeadersBuilder {
 func (r *httpHeadersBuilder) Set(name string, values ...string) *httpHeadersBuilder {
 	// If the named header is not set, then create it
 
-	// FIXME: This implementation is wrong. originally it created 0-length then appended values.
-	// Now it creates exact length and then sets, but doesn't account for new values with append
-	if _, ok := (*r.headers)[name]; !ok {
-		(*r.headers)[name] = make([]string, len(values))
-	}
-
-	// FIXME: This is unreachable code, it will never be !ok here:
 	if _, ok := (*r.headers)[name]; !ok {
 		(*r.headers)[name] = make([]string, 0)
 	}
 
-	// FIXME: How do we append new values to existing ones? This is wrong!
-	for i, value := range values {
-		(*r.headers)[name][i] = value
+	for _, value := range values {
+		(*r.headers)[name] = append((*r.headers)[name], value)
 	}
 	return r
 }
