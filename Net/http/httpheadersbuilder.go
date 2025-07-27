1dd6662bec5e52665b54be0c621fa70d2614eb6f
Sean Kelly <keenskelly@gmail.com>
2025-07-26 21:18:37 -0700

    WIP - Support set/override in addition to add/merge for HeadersBuilder

--- Net/http/httpheadersbuilder.go
+++ Net/http/httpheadersbuilder.go
@@ -5,11 +5,12 @@ package http
 Build a set of HTTP Headers for Request or Response
 
 TODO:
- * Consider direct support with validation for specific, known header names
-   ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers
- * Also validate custom header names against RFC 7230 for acceptable characters < 40 length
-   ref: https://datatracker.ietf.org/doc/html/rfc7230
- * Deduplicate provided values for Set() against existing ones
+  * Consider direct support with validation for specific, known header names
+    ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers
+  * Also validate custom header names against RFC 7230 for acceptable characters < 40 length
+    ref: https://datatracker.ietf.org/doc/html/rfc7230
+  * Deduplicate provided values for Set() against existing ones
+  * Merge() may need to optionally overwrite existing values instead of append
 */
 
 // Name/value pair header map for Request or Response
@@ -18,8 +19,10 @@ type httpHeadersBuilder struct {
 }
 
 type HttpHeadersBuilderIfc interface {
-	Set(name string, values ...string)
-	Merge(headers HttpHeadersIfc)
+	Set(name string, values ...string) *httpHeadersBuilder
+	Append(name string, values ...string) *httpHeadersBuilder
+	Merge(headers HttpHeadersIfc) *httpHeadersBuilder
+	Override(headers HttpHeadersIfc) *httpHeadersBuilder
 	GetHttpHeaders() *httpHeaders
 }
 
@@ -42,8 +45,13 @@ func NewHttpHeadersBuilder() *httpHeadersBuilder {
 
 // Single-name, multi-value support
 func (r *httpHeadersBuilder) Set(name string, values ...string) *httpHeadersBuilder {
-	// If the named header is not set, then create it
+	// Create the named header (and clear the value!)
+	r.headers.headers[name] = make([]string, 0)
+	return r.Append(name)
+}
 
+func (r *httpHeadersBuilder) Append(name string, values ...string) *httpHeadersBuilder {
+	// If the named header is not set, then create it
 	if _, ok := r.headers.headers[name]; !ok {
 		r.headers.headers[name] = make([]string, 0)
 	}
@@ -59,6 +67,24 @@ func (r *httpHeadersBuilder) Merge(headers *httpHeaders) *httpHeadersBuilder {
 		for name, values := range headers.headers {
 			// Use Set() to merge provided values with existing, instead of overwriting
 			r.Set(name, values...)
+		}
+	}
+	return r
+}
+
+func (r *httpHeadersBuilder) Override(headers HttpHeadersIfc) *httpHeadersBuilder {
+	if headers != nil {
+		names := headers.GetNames()
+		if names != nil {
+			for index := range *names {
+				name := (*names)[index]
+				// Directly set the header, overwriting any existing values
+				values := headers.Get(name)
+				if values == nil { // If no values, then skip
+					continue
+				}
+				r.Set(name, *values...)
+			}
 		}
 	}
 	return r
